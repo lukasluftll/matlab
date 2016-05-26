5bbfb2431698cf8ef6b062aed5497779c9ac45ef
Alexander Schaefer <fudger@users.noreply.github.com>
2016-05-26 10:14:56 +0200

    Simplify code.

--- pcd/plot/cube.m
+++ pcd/plot/cube.m
@@ -7,15 +7,17 @@ function cube(center, edgeLength, varargin)
 %   varargin. For possible name-value pairs, see the documentation of 
 %   function PATCH.
 %
-%   Example
+%   Example:
 %      cube([0, 5, 3], 1, 'FaceColor', 'yellow');
 %
+%   See also PATCH.
+
 % Copyright 2016 Alexander Schaefer
 
 % TODO
 % + Specify face color.
 % - Vectorize input.
-% - Use face-vertex arguments of patch.
+% + Use face-vertex arguments of patch.
 
 %% Validate input.
 % Check if the user provided enough input arguments.
@@ -24,14 +26,14 @@ if nargin < 2
 end
 
 %% Plot the cube.
-% Compute the corner coordinates.
-corner = center - [edgeLength/2, edgeLength/2, edgeLength/2];
-corner = [corner; corner + repmat([edgeLength, 0, 0], 1, 1)];
-corner = [corner; corner + repmat([0, edgeLength, 0], 2, 1)];
-corner = [corner; corner + repmat([0, 0, edgeLength], 4, 1)];
+% Compute the vertices.
+vertices = center - [edgeLength/2, edgeLength/2, edgeLength/2];
+vertices = [vertices; vertices + repmat([edgeLength, 0, 0], 1, 1)];
+vertices = [vertices; vertices + repmat([0, edgeLength, 0], 2, 1)];
+vertices = [vertices; vertices + repmat([0, 0, edgeLength], 4, 1)];
 
 % Define all combinations of the corners to form all 6 faces.
-combination = [1, 2, 4, 3; ...
+faces = [1, 2, 4, 3; ...
     1, 2, 6, 5; ...
     2, 4, 8, 6; ...
     4, 3, 7, 8; ...
@@ -39,9 +41,6 @@ combination = [1, 2, 4, 3; ...
     5, 6, 8, 7];
 
 % Plot all faces.
-for c = 1 : size(combination, 1)
-    face = corner(combination(c,:), :);
-    patch(face(:,1), face(:,2), face(:,3), varargin{:});
-end
+patch('Faces', faces, 'Vertices', vertices, varargin{:});
 
 end
