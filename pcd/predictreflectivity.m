84f48194bec3482c291290585b3be225fb3330aa
Alexander Schaefer <fudger@users.noreply.github.com>
2016-05-26 13:57:05 +0200

    Generate hits vs. misses voxel map.

--- pcd/predictreflectivity.m
+++ pcd/predictreflectivity.m
@@ -1,91 +1,114 @@
 % Voxelizes the volume occupied by a Lidar scan and computes for each
 % voxel the reflection probability.
+% The reflection probability is computed under the assumption that the 
+% points in each voxel are Gauss-distributed.
 
 % Define the voxel map resolution.
-res = 1.0;
+res = 0.5;
 
 % Read the point cloud.
-cloud = pcdread('data/office.pcd');
-pointsCart = cloud.pointCloud.Location;
+cloud = pcdread('data/campus.pcd');
+points = cloud.pointCloud.Location;
 
 % Compute the spherical coordinates of the points w.r.t. the sensor frame.
-beamsCart = pointsCart ...
-    - repmat(cloud.viewpoint.T(4,1:3), size(pointsCart, 1), 1);
-[azimuth, elevation, r] = cart2sph(...
-    beamsCart(:,1), beamsCart(:,2), beamsCart(:,3));
-beamsSph = [azimuth, elevation, r];
+beams = pc2sph(points);
 
 % Compute the volume occupied by the Lidar scan.
 limits = [cloud.pointCloud.XLimits; ...
     cloud.pointCloud.YLimits; ...
     cloud.pointCloud.ZLimits];
-limits = [floor(limits(:,1)), ceil(limits(:,2))];    
+
+% Make sure the limits are multiples of the resolution so the voxels
+% are alinged along the planes spanned by the axis pairs.
+limits = [floor(limits(:,1) / res) * res, ceil(limits(:,2) / res) * res];
+
+% Compute the diameter of the point cloud.
+diameter = norm(limits(:,2) - limits(:,1));
 
 % Create the figure where to plot the occupancy map.
 fig = figure('Name', 'Occupancy map', 'NumberTitle', 'Off');
+grid on;
+axis equal;
 
-% Iterate over all voxels.
-for (x = limits(1,1) : res : limits(1,2))
-    for (y = limits(2,1) : res : limits(2,2))
-        for (z = limits(3,1) : res : limits(3,2))
+% Iterate over all voxels and for each one compute the reflection 
+% probability.
+for x = limits(1,1) : res : limits(1,2)
+    for y = limits(2,1) : res : limits(2,2)
+        for z = limits(3,1) : res : limits(3,2)
             % Compute the limits of the voxel.
-            roi = [x, x+res; y, y+res; z, z+res];
+            voxel = [x, x+res; y, y+res; z, z+res];
             
-            % Find all points living in the voxel.
-            roiIndices = findPointsInROI(cloud.pointCloud, roi);
-            roiCartPoints = pointsCart(roiIndices,:);
-            hits = length(roiIndices);
+            % Find all points that reside inside the voxel.
+            roiIndices = findPointsInROI(cloud.pointCloud, voxel);
+            hits = numel(roiIndices);
+            cloudSize = size(cloud.pointCloud.Location);
+            [row, col] = ind2sub(cloudSize(1:2), roiIndices);
+            roiPoints = points(row,col,:);
             
-            % Compute the coordinates of all 8 voxel corners.
-            voxelCorners = [x, y, z];
-            voxelCorners = [voxelCorners; ...
-                voxelCorners + repmat([res, 0, 0], 1, 1)];
-            voxelCorners = [voxelCorners; ...
-                voxelCorners + repmat([0, res, 0], 2, 1)];
-            voxelCorners = [voxelCorners; ...
-                voxelCorners + repmat([0, 0, res], 4, 1)];
+            % Compute the coordinates of all 8 voxel vertices.
+            vertices = [x, y, z];    %#ok<*AGROW>
+            vertices = [vertices; vertices + repmat([res, 0, 0], 1, 1)]; 
+            vertices = [vertices; vertices + repmat([0, res, 0], 2, 1)];
+            vertices = [vertices; vertices + repmat([0, 0, res], 4, 1)];
             
-            % Use the voxel corners to compute the minimum and maximum 
-            % azimuth and elevation angles of beams that traverse the 
-            % voxel.
+            % Use the vertices to compute the minimum and maximum 
+            % azimuth and elevation angles of the beams that penetrate
+            % the voxel.
             [azimuth, elevation, r] = cart2sph(...
-                voxelCorners(:,1), voxelCorners(:,2), voxelCorners(:,3));
+                vertices(:,1), vertices(:,2), vertices(:,3));
             
             % Make sure the azimuth difference for the voxel corners is
             % correctly computed if the voxel touches the negative x-axis;
-            % there, the azimuth angle changes from +pi to -pi.
-            if (min(voxelCorners(:,1)) < 0 && max(voxelCorners(:,2)) == 0)
+            % at the negative x-axis, the azimuth angle changes 
+            % from +pi to -pi.
+            if (min(vertices(:,1)) < 0 && max(vertices(:,2)) == 0)
                 azimuth(azimuth == pi) = -pi;
             end
             
-            % Delete all beams that cannot dip in the voxel volume.
-            travbeams = beamsSph;
-            travbeams(travbeams(:,1) < min(azimuth), :) = [];
-            travbeams(travbeams(:,1) > max(azimuth), :) = [];
-            travbeams(travbeams(:,2) < min(elevation), :) = [];
-            travbeams(travbeams(:,2) > max(elevation), :) = [];
-            travbeams(travbeams(:,3) < min(r), :) = [];
+            % Find the logical subscripts of all beams that can 
+            % permeate the voxel volume.
+            del = logical(...
+                beams(:,:,1) < min(azimuth) ...
+                + beams(:,:,1) > max(azimuth) ...
+                + beams(:,:,2) < min(elevation) ...
+                + beams(:,:,2) > max(elevation) ...
+                + beams(:,:,3) < min(r));
             
-            % Find out how many beams dipped into the voxel volume.
-            dips = 0;
-            for (b = 1 : size(travbeams, 1))
-                % Compute the intersection of the beam and the voxel.
-                origin = zeros(1, 3);
-                direction = sph2cart(travbeams(b,:));
-                vmin = roi(:,1);
-                vmax = roi(:,2);
-                [flag, tmin] = rayBoxIntersection(...
-                    origin, direction, vmin, vmax);
-                
-                % Check whether the beam dipped into the voxel volume.
-                if (tmin < 1)
-                    dips = dips + 1;
+            % Keep only those beams that can permeate in the voxel volume.
+            permbeams = beams;
+            permbeams(cat(3, del, del, del)) = [];
+            
+            % Find out how many beams really permeate the voxel volume.
+            perms = 0;
+            for row = 1 : size(permbeams, 1)
+                for col = 1 : size(permbeams, 2)
+                    % NaN beams are infinitely long; set their radius to 
+                    % the diameter of the point cloud.
+                    if isnan(permbeams(row,col,3))
+                        permbeams(row,col,3) = diameter;
+                    end
+
+                    % Compute the intersection of the beam and the voxel.
+                    origin = zeros(1, 3);
+                    [dirx, diry, dirz] = sph2cart(permbeams(row,col,1), ...
+                        permbeams(row,col,2), permbeams(row,col,3));
+                    vmin = voxel(:,1);
+                    vmax = voxel(:,2);
+                    [flag, tmin] = rayBoxIntersection(...
+                        origin, [dirx, diry, dirz], vmin, vmax);
+
+                    % Check whether the beam permeates the voxel volume.
+                    if (tmin < 1)
+                        perms = perms + 1;
+                    end
                 end
             end
             
-            % Plot the cube.
-            if (dips < 1)
-                cube([x + res/2, y + res/2, z + res/2], res);
+            % Plot the voxel.
+            if (hits > 1)
+                cube([x + res/2, y + res/2, z + res/2], res, ...
+                    'FaceColor', 'red', 'FaceAlpha', 0.5);
+                drawnow;
             end            
         end
     end
