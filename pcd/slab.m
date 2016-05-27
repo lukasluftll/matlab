e53d1f1e9c1bf0ee0effc3c752954c79cf17700f
Alexander Schaefer <fudger@users.noreply.github.com>
2016-05-27 23:53:37 +0200

    Define t correctly in case of no intersection.

--- pcd/slab.m
+++ pcd/slab.m
@@ -1,33 +1,33 @@
-function [intersects, t] = slab(origin, ray, box)
+function [intersects, t] = slab(support, ray, box)
 % SLAB Compute intersection of ray with axis-aligned box in 3D.
-%   INTERSECTS = SLAB(ORIGIN, RAY, BOX) returns whether the infinite ray 
-%   characterized by ORIGIN and RAY intersects with the 3D axis-aligned box 
-%   described by BOX. 
+%   INTERSECTS = SLAB(SUPPORT, RAY, BOX) returns whether the infinite ray 
+%   characterized by SUPPORT and RAY intersects with the 3D axis-aligned 
+%   box described by BOX. 
 %
-%   ORIGIN and RAY are 3-element column vectors. ORIGIN contains the
-%   coordinates of the ray's origin, RAY indicates the direction of the
-%   ray. BOX is a 3x2 matrix whose columns contain the minimum and
+%   SUPPORT and RAY are 3-element column vectors. SUPPORT contains the
+%   coordinates of the ray's point of support, RAY indicates the direction 
+%   of the ray. BOX is a 3x2 matrix whose columns contain the minimum and
 %   maximum coordinates of the box. The rows correspond to the coordinates.
 %
-%   [INTERSECTS, T] = SLAB(ORIGIN, RAY, BOX) also returns 
-%   a 2-element column vector. ORIGIN + T(1)*RAY is the coordinate of the 
-%   point where the ray enters the box, ORIGIN + T(2)*RAY is the point
+%   [INTERSECTS, T] = SLAB(SUPPORT, RAY, BOX) also returns 
+%   a 2-element row vector. SUPPORT + T(1)*RAY is the coordinate of the 
+%   point where the ray enters the box, SUPPORT + T(2)*RAY is the point
 %   where the ray leaves the box. If the ray does not intersect with the 
-%   box, T is [-inf; +inf].
+%   box, T is NaN.
 %
 %   Example:
-%   origin = zeros(3, 1);
+%   support = zeros(3, 1);
 %   ray = ones(3, 1);
 %   box = [2*ones(3, 1), 3*ones(3, 1)];
-%   [intersects, t] = slab(origin, ray, box)
+%   [intersects, t] = slab(support, ray, box)
 %
-%   See also INF.
+%   See also NAN.
 
 % Copyright 2016 Alexander Schaefer
 
 % Compute the line parameters of the intersections with the 
 % pairwise parallel infinite planes that confine the box. 
-t = (box - [origin, origin]) ./ [ray, ray];
+t = sort((box - [support, support]) ./ [ray, ray], 2);
 
 % Compute the parameters corresponding to the points where ray enters the
 % box and where it leaves it.
@@ -36,5 +36,6 @@ t = [max(t(:,1)), min(t(:,2))];
 % Check whether some part of the ray remains after computing the entry 
 % and leaving point.
 intersects = diff(t) >= 0;
+t([intersects, intersects]) = NaN;
 
 end
