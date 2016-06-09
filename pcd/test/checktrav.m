86199854553b5d4dea090b79464e69e0d6758e2c
Alexander Schaefer <aschaef@cs.uni-freiburg.de>
2016-06-09 09:39:30 +0200

    Add check for zero indices.

--- pcd/test/checktrav.m
+++ pcd/test/checktrav.m
@@ -3,6 +3,9 @@ function checktrav(i, iExpected, t, tExpected)
 %
 %   See also TRAV.
 
+% Check number of input arguments.
+narginchk(1, 4);
+
 % Define the exceptions.
 iSizeExcpt = MException('trav:iSize', ...
     'Incorrect index matrix size.');
@@ -12,15 +15,21 @@ iIncorrectExcpt = MException('trav:iIncorrect', ...
     'Returned indices incorrect.');
 tIncorrectExcpt = MException('trav:tIncorrect', ...
     'Returned line parameters incorrect.');
+iZeroExcpt = MException('trav:iZero', ...
+    'Returned indices contain zero values.');
 
 % Check results.
-assert(all(size(i) == size(iExpected)), iSizeExcpt.identifier, ...
-    iSizeExcpt.message);
-assert(all(size(t) == size(tExpected)), tSizeExcpt.identifier, ...
-    tSizeExcpt.message);
-assert(all(all(i - iExpected < eps)), iIncorrectExcpt.identifier, ...
-    iIncorrectExcpt.message);
-assert(all(t - tExpected < eps | (isnan(t) & isnan(tExpected))), ...
-    tIncorrectExcpt.identifier, tIncorrectExcpt.message);
+assert(all(all(i)), iZeroExcpt.identifier, iZeroExcpt.message);
+
+if nargin > 1
+    assert(all(size(i) == size(iExpected)), iSizeExcpt.identifier, ...
+        iSizeExcpt.message);
+    assert(all(size(t) == size(tExpected)), tSizeExcpt.identifier, ...
+        tSizeExcpt.message);
+    assert(all(all(i - iExpected < eps)), iIncorrectExcpt.identifier, ...
+        iIncorrectExcpt.message);
+    assert(all(t - tExpected < eps | (isnan(t) & isnan(tExpected))), ...
+        tIncorrectExcpt.identifier, tIncorrectExcpt.message);
+end
 
 end
