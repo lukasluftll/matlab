d0b37f842e20dbdfed34c2582d500c90f958529e
Alexander Schaefer <fudger@users.noreply.github.com>
2016-06-13 23:51:58 +0200

    Return struct instead of pointCloud object; consider COUNT.

--- pcd/pcdread.m
+++ pcd/pcdread.m
@@ -1,79 +1,65 @@
-function outcloud = pcdread(filename)
-% PCDREAD Read a 3D point cloud from PCD file.
-%   OUTCLOUD = PCDREAD(FILENAME) reads a point cloud from the 
-%   PCD file specified by the string filename.
-%   If a accompanying DAT file is available, this function also reads 
-%   the DAT file's contents.
+function data = pcdread(filename)
+% PCDREAD Read contents of point cloud from PCD file.
+%   DATA = PCDREAD(FILENAME) reads all data fields of the PCD file defined 
+%   by FILENAME and returns them in a struct DATA.
+%   If an accompanying DAT file is available, its contents are also added
+%   to DATA.
 %
-%   In order for the DAT file to be read, both files must stay with the 
-%   following naming convention:
+%   DAT file naming convention
+%   --------------------------
+%   The DAT file must obey the following naming convention:
 % 
-%   PCD file name   <name>.pcd
-%   DAT file name   <name>_info.dat
+%   PCD file name   <filename>.pcd
+%   DAT file name   <filename>_info.dat
 %
-%   The return value OUTCLOUD is a structure with the following elements:
+%   Recognized field names
+%   ----------------------
+%   PCDREAD recognizes the following data field names in the PCD file 
+%   header and returns the corresponding data types:
 %
-%   pointCloud      pointCloud object read from the PCD file
+%   'viewpoint'     affine3d object that represents the sensor frame 
+%                   defined in the PCD file
 %
-%   intensity       matrix that contains the intensity values stored in the 
-%                   field 'intensity' in the PCD file
+%   'odometry'      affine3d object that represents the odometry pose 
+%                   defined in the DAT file
 %
-%   viewpoint       affine3d object that describes the sensor coordinate
-%                   frame stores in the PCD file
-%  
-%   time            time stamp stored in the DAT file
+%   'gps'           structure array that represents the GPS coordinates 
+%                   of the DAT file; it contains the following elements:
+%                   'time'            time stamp of the GPS reading
+%                   'longitude'       GPS	longitude
+%                   'latitude'        GPS latitude
+%                   'elevation'       GPS elevation
+% 
+%   Organized vs. unorganized point clouds
+%   --------------------------------------
+%   If the PCD file header specifies a height value of 1, the point cloud
+%   is unorganized. In this case, the values of the data fields are stored 
+%   in matrices of size 1xWIDTHxCOUNT, with WIDTH being the width of the 
+%   point cloud and COUNT being the data element count from the PCD file 
+%   header. 
 %
-%   odometry        affine3d object that describes the odometry pose stored
-%                   in the DAT file
-%
-%   gps             structure that stores the GPS coordinate read from the
-%                   DAT file; its elements are:
-%                   time            time stamp of the GPS reading
-%                   longitude       GPS	longitude
-%                   latitude        GPS latitude
-%                   elevation       GPS elevation
-%
+%   If the specified height value is greater than 1, the point cloud is
+%   organized and the values are stored in matrices of size
+%   HEIGHTxWIDTHxCOUNT.
+% 
 %   PCD file format
 %   ---------------
 %   PCD files occur in different formats: ASCII and binary. This function
 %   only reads ASCII files.
-%
-%   Relevant PCD file contents
-%   --------------------------
-%   PCD files as defined in the Point Cloud Library can contain numerous 
-%   data entries. pcdread loads the following fields, if given: 
-%   x, y, z                       positions of the points
-%   r, g, b                       colors of the points
-%   normal_x, normal_y, normal_z  normals of the points
-%   intensity                     remission values of the points
-%
-%   The data entries can be accessed as follows:
-%   x, y, z                       outcloud.pointCloud.Location
-%   r, g, b                       outcloud.pointCloud.Color
-%   normal_x, normal_y, normal_z  outcloud.pointCloud.Normal
-%   intensity                     outcloud.intensity
-%
-%   Organized vs. unorganized point clouds
-%   --------------------------------------
-%   If the PCD file header specifies a height value of 1, the point cloud
-%   is unorganized. In this case, the points' positions, colors, and 
-%   normals are stored in matrices of size [height * width, 3]. 
-%   intensity is a matrix of size [height * width, 1].
-%
-%   If the specified height value is greater than 1, the point cloud is
-%   organized and the values are stored in matrices of size 
-%   [height, width, 3] and [height, width, 1], respectively.
-% 
-%   Example : read a point cloud from a PCD file
-%   --------------------------------------------
-%   outcloud = pcdread('terrain.pcd');
-%   pcshow(outcloud.pointCloud);
+%  
+%   Example:
+%      data = pcdread('office.pcd');
+%      cloud = pointCloud([data.x; data.y; data.z]');
+%      pcshow(cloud);
 %
 %   See also PCREAD, PTSREAD, POINTCLOUD, PCWRITE, PCSHOW.
  
-%  Copyright 2016 Alexander Schaefer
+% Copyright 2016 Alexander Schaefer
 
 %% Validate PCD file name.
+% Check the number of input arguments.
+narginchk(1, 1);
+
 % Make sure the given file name is a string.
 if ~ischar(filename)
     error(message('vision:pointcloud:badFileName'));
@@ -81,7 +67,7 @@ end
 
 % Verify that the file exists.
 fid = fopen(filename, 'r');
-if (fid == -1)
+if fid == -1
     if isempty(dir(filename))
         error(message('MATLAB:imagesci:imread:fileDoesNotExist', ...
               filename));
@@ -96,204 +82,121 @@ end
 fgetl(fid);
 
 % PCD file version.
-pcdVersionString = fgetl(fid);
-pcdVersionString = pcdVersionString(length('VERSION')+1 : end);
-pcdVersion = textscan(pcdVersionString, '%f');
-pcdVersion = pcdVersion{1};
+pcdversion = fgetl(fid);
+pcdversion = pcdversion(length('VERSION')+1:end);
+pcdversion = cell2mat(textscan(pcdversion, '%f'));
 
 % Fields contained in the PCD file.
-fieldsString = fgetl(fid);
-fieldsString = fieldsString(length('FIELDS')+1 : end);
-fields = textscan(fieldsString, '%s');
-fields = fields{1};
+fieldname = fgetl(fid);
+fieldname = fieldname(length('FIELDS')+1:end);
+fieldname = textscan(fieldname, '%s');
+fieldname = fieldname{:};
 
 % Size of each field in bytes.
-fieldSizeString = fgetl(fid);
-fieldSizeString = fieldSizeString(length('SIZE')+1 : end);
-fieldSize = textscan(fieldSizeString, '%d');
-fieldSize = fieldSize{1};
+fieldsize = fgetl(fid);
+fieldsize = fieldsize(length('SIZE')+1:end);
+fieldsize = cell2mat(textscan(fieldsize, '%d'));
 
 % Data type of each field.
-fieldTypeString = fgetl(fid);
-fieldTypeString = fieldTypeString(length('TYPE')+1 : end);
-fieldType = textscan(fieldTypeString, '%c');
-fieldType = fieldType{1};
+fieldtype = fgetl(fid);
+fieldtype = fieldtype(length('TYPE')+1:end);
+fieldtype = cell2mat(textscan(fieldtype, '%c'));
 
-% Number of elements per dimension.
-countString = fgetl(fid);
-countString = countString(length('COUNT')+1 : end);
-count = textscan(countString, '%d');
-count = count{1};
+% Number of elements per field per point.
+fieldcount = fgetl(fid);
+fieldcount = fieldcount(length('COUNT')+1:end);
+fieldcount = cell2mat(textscan(fieldcount, '%d'));
 
 % Point cloud width.
-widthString = fgetl(fid);
-widthString = widthString(length('WIDTH')+1 : end);
-width = textscan(widthString, '%d');
-width = width{1};
+width = fgetl(fid);
+width = width(length('WIDTH')+1:end);
+width = cell2mat(textscan(width, '%d'));
 
 % Point cloud height.
-heightString = fgetl(fid);
-heightString = heightString(length('HEIGHT')+1 : end);
-height = textscan(heightString, '%d');
-height = height{1};
+height = fgetl(fid);
+height = height(length('HEIGHT')+1:end);
+height = cell2mat(textscan(height, '%d'));
 
-% Acquisition viewpoint for the points in the dataset.
-viewpointString = fgetl(fid);
-viewpointString = viewpointString(length('VIEWPOINT')+1 : end);
-viewpoint = textscan(viewpointString, '%f');
-viewpoint = viewpoint{1};
+% Acquisition viewpoint of the points in the dataset.
+viewpoint = fgetl(fid);
+viewpoint = viewpoint(length('VIEWPOINT')+1:end);
+viewpoint = cell2mat(textscan(viewpoint, '%f'));
 
 % Total number of points.
-pointsString = fgetl(fid);
-pointsString = pointsString(length('POINTS')+1 : end);
-points = textscan(pointsString, '%d');
-points = points{1};
+count = fgetl(fid);
+count = count(length('POINTS')+1:end);
+count = cell2mat(textscan(count, '%d'));
 
 % Data type of the PCD file.
-dataString = fgetl(fid);
-dataString = dataString(length('DATA')+1 : end);
-data = textscan(dataString, '%s');
-data = data{1};
+pcdtype = fgetl(fid);
+pcdtype = pcdtype(length('DATA')+1:end);
+pcdtype = textscan(pcdtype, '%s');
+pcdtype = pcdtype{:};
 
 fclose(fid);
 
 %% Check file and read payload data.
 % Warn if the PCD file version is older than the official entry point for
 % the PCD file format.
-if (pcdVersion < 0.7)
-    warning('PCD file version is below 0.7 and thus not well defined.');
+if pcdversion < 0.7
+    warning('PCD file version is below 0.7: file format not well defined.')
 end
 
 % Make sure the PCD file's data type is ASCII. 
-if (~strcmpi(data, 'ascii'))
-    error('This function can only read PCD files of type ASCII.');
+if ~strcmpi(pcdtype, 'ascii')
+    error('This function only reads PCD files of type ''ascii''.')
 end
 
-% Read the data.
-data = dlmread(filename, ' ', 11, 0);
+% If the number of all points does not equal the point cloud height x
+% width, issue a warning.
+if count ~= width * height
+    warning('Inconsistent PCD header fields: POINTS ~= WIDTH * HEIGHT.')
+end
 
-%% Read sensor viewpoint.
-% Convert the quaternion to a rotation matrix.
+% Read raw data.
+rawdata = dlmread(filename, ' ', 11, 0);
+
+%% Read payload data.
+% Convert raw data to cell array. Each cell element contains a matrix of
+% size WIDTH x HEIGHT x FIELDCOUNT.
+data = cell(1, length(fieldcount));
+col = [0; cumsum(fieldcount)];
+for i = 1 : length(fieldcount)
+    data{i} = rawdata(:, col(i)+1:col(i+1));
+    data{i} = reshape(data{i}, width, height, fieldcount(i));
+    data{i} = permute(data{i}, [2 1 3]);
+end
+
+% Convert data to the data types specified in the PCD file header.
+uint = regexp(fieldtype', '[uU]');
+data(uint) = cellfun(@uint32, data(uint), 'UniformOutput', false);
+int = regexp(fieldtype', '[iI]');
+data(int) = cellfun(@int32, data(int), 'UniformOutput', false);
+
+% Transform the cell array into a structure array.
+data = cell2struct(data', fieldname, 1);
+
+%% Append viewpoint to return structure.
 viewpointMatrix = quat2rotm(viewpoint(4:7)');
-
-% Add the translation vector.
 viewpointMatrix(:, 4) = viewpoint(1:3)';
 viewpointMatrix(4, :) = [0, 0, 0, 1];
+data.viewpoint = affine3d(viewpointMatrix');
 
-% Construct the affine3d object.
-viewpoint = affine3d(viewpointMatrix');
+%% Read DAT file and append information to return structure.
+% Create the DAT file name following naming convention.
+datFilename = [filename(1:end-length('.pcd')), '_info.dat'];
 
-%% Read locations.
-% Check if the data contains the point coordinates.
-if (isempty(strmatch('x', fields)) ...
-        || isempty(strmatch('y', fields)) ...
-        || isempty(strmatch('z', fields)))
-    error('PCD file does not contain fields named x, y, z.');
-end
-
-% Get the coordinates of the points.
-x = data(:, strmatch('x', fields)); %#ok<*MATCH2>
-y = data(:, strmatch('y', fields));
-z = data(:, strmatch('z', fields));
-
-% In case the point cloud is ordered, save the point coordinates to a 
-% 3D matrix.
-if (height > 1)
-    x = reshape(x, width, height); 
-    y = reshape(y, width, height);
-    z = reshape(z, width, height);
-    location = cat(3, x', y', z');
-else
-    location = [x, y, z];
-end
-
-%% Read intensities.
-% Check if the file contains intensity values.
-intensity = [];
-if (~isempty(strmatch('intensity', fields)))
-    % Read the intensity values.
-    intensity = data(:, strmatch('intensity', fields));
-    
-    % Organize the values, if the point cloud is organized.
-    if (height > 1)
-        intensity = reshape(intensity, width, height)';
-    end
-end
-
-%% Read colors.
-% Check if the file contains color values.
-color = [];
-if (~isempty(strmatch('r', fields)) ...
-        && ~isempty(strmatch('g', fields)) ...
-        && ~isempty(strmatch('b', fields)))
-    % Read the color values.
-    r = uint8(data(:, strmatch('r', fields)));
-    g = uint8(data(:, strmatch('g', fields)));
-    b = uint8(data(:, strmatch('b', fields)));
-    
-    % Organize the values, if the point cloud is organized.
-    if (height > 1)
-        r = reshape(r, height, width);
-        g = reshape(g, height, width);
-        b = reshape(b, height, width);
-        color = cat(3, r, g, b);
-    else
-        color = [r, g, b];
-    end
-end
-
-%% Read normals.
-% Check if the file contains normals.
-normal = [];
-if (~isempty(strmatch('normal_x', fields)) ...
-        && ~isempty(strmatch('normal_y', fields)) ...
-        && ~isempty(strmatch('normal_z', fields)))
-    % Read the normal vectors.
-    normal_x = data(:, strmatch('normal_x', fields));
-    normal_y = data(:, strmatch('normal_y', fields));
-    normal_z = data(:, strmatch('normal_z', fields));
-    
-    % Organize the values, if the point cloud is organized.
-    if (height > 1)
-        normal_x = reshape(normal_x, height, width);
-        normal_y = reshape(normal_y, height, width);
-        normal_z = reshape(normal_z, height, width);
-        normal = cat(3, normal_x, normal_y, normal_z);
-    else
-        normal = [normal_x, normal_y, normal_z];
-    end
-end
-
-%% Read DAT file.
-% Create the DAT file's name.
-datFilename = [filename(1 : end-length('.pcd')), '_info.dat'];
-
-% Verify that the file exists.
+% Read position information.
 fid = fopen(datFilename, 'r');
-position = [];
-if (fid ~= -1)
-    position = posread(datFilename);
-end
-       
-%% Create the return structure.
-outcloud.viewpoint = viewpoint;
-outcloud.intensity = intensity;
-
-if (~isempty(position))
-    outcloud.time = position.time;
-    outcloud.odometry = position.odometry;
-    outcloud.gps = position.gps;
+pos = [];
+if fid ~= -1
+    pos = posread(datFilename);
 end
 
-if (isempty(color) && isempty(normal))
-    outcloud.pointCloud = pointCloud(location);
-elseif (~isempty(color) && isempty(normal))
-    outcloud.pointCloud = pointCloud(location, 'Color', color);
-elseif (isempty(color) && ~isempty(normal))
-    outcloud.pointCloud = pointCloud(location, 'Normal', normal);
-else
-    outcloud.pointCloud = pointCloud(location, ...
-                                     'Color', color, 'Normal', normal);
+% Append position information to return structure.
+posfield = fieldnames(pos);
+for i = 1 : numel(posfield)
+    data.(posfield{i}) = pos.(posfield{i});
 end
 
 end
