287655e59d8f651b2b70ec9462ffc9494234f0e7
Alexander Schaefer <fudger@users.noreply.github.com>
2016-06-14 22:40:46 +0200

    Update.

--- pcd/scripts/vizlambda.m
+++ pcd/scripts/vizlambda.m
@@ -1,8 +1,25 @@
+% Load the point cloud data.
 cloud = pcdread('data/castle.pcd');
-cloud = cloud.pointCloud;
 
-res = 10;
+% Define the voxel resolution used to compute the normal distributed ray 
+% decay rates.
+calcres = 5;
+
+% Calculate the ray decay rate.
+lambda = raydecay(cloud.azimuth, cloud.elevation, cloud.radius, calcres);
+
+% Perform normal distribution transformation.
+[mu, sigma] = ndt(cloud, calcres);
+
+% Define the voxel resolution used to visualize the decay rate
+% distribution.
+vizres = 1;
+
+% Visualize the sum of the normal distributions of the decay rates of 
+% all voxels.
+mvnpdf(mu, sigma)
+calcres = 5;
 
 ray = pc2sph(cloud.Location, 'vlp16');
-lambda = raydecay(cloud, res);
-[mu, sigma] = pc2gd(cloud, res);
+lambda = raydecay(cloud, calcres);
+[mu, sigma] = pc2gd(cloud, calcres);
