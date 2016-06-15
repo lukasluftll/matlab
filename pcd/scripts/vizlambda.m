4144b60086707b2c551e25ab8fbcd5e1a9d8fcfe
Alexander Schaefer <aschaef@cs.uni-freiburg.de>
2016-06-15 09:22:31 +0200

    Implement computation of sum of normal distribution.

--- pcd/scripts/vizlambda.m
+++ pcd/scripts/vizlambda.m
@@ -1,25 +1,44 @@
 % Load the point cloud data.
-cloud = pcdread('data/castle.pcd');
+data = pcdread('data/castle.pcd');
+
+% Create pointCloud object.
+cloud = pointCloud(cat(3, data.x, cat(3, data.y, data.z)));
 
 % Define the voxel resolution used to compute the normal distributed ray 
 % decay rates.
-calcres = 5;
+calcres = 1;
+
+% Compute the axis-aligned volume spanned by the point cloud.
+vol = [min(data.x(:)), min(data.y(:)), min(data.z(:)), ...
+        max(data.x(:)), max(data.y(:)), max(data.z(:))];
+
+% Make sure the points in the maximum plane of the volume are part of the
+% volume.
+vol(4:6) = vol(4:6) + eps(vol(4:6));
+vol = [0, 0, 0, 2, 1, 1];
 
 % Calculate the ray decay rate.
-lambda = raydecay(cloud.azimuth, cloud.elevation, cloud.radius, calcres);
+lambda = raydecay(data.azimuth, data.elevation, data.radius, ...
+    calcres, vol);
 
 % Perform normal distribution transformation.
-[mu, sigma] = ndt(cloud, calcres);
-
+[mu, sigma] = ndt(cloud, calcres, vol);
+muLin = reshape(mu(:), [], 3);
+sigmaLin = reshape(sigma(:), [], 3, 3);
+sigmaLin = permute(sigmaLin, [2, 3, 1]);
+            
 % Define the voxel resolution used to visualize the decay rate
 % distribution.
 vizres = 1;
 
-% Visualize the sum of the normal distributions of the decay rates of 
-% all voxels.
-mvnpdf(mu, sigma)
-calcres = 5;
-
-ray = pc2sph(cloud.Location, 'vlp16');
-lambda = raydecay(cloud, calcres);
-[mu, sigma] = pc2gd(cloud, calcres);
+% Iterate over all voxels and compute sum of all normal distributions for 
+% each voxel.
+for x = 1 : size(lambda, 1)
+    for y = 1 : size(lambda, 2)
+        for z = 1 : size(lambda, 3)
+            % Compute the center of the voxel.
+            c = vol(1:3) + ([x, y, z] - 0.5*ones(1, 3)) * calcres;
+            p(x,y,z) = mvnpdf(c, muLin, sigmaLin) .* lambda(:);
+        end
+    end
+end
