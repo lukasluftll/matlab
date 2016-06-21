3e76b134a7c6b07e93cfee77bc95202c49b5f194
Alexander Schaefer <aschaef@cs.uni-freiburg.de>
2016-06-21 15:35:37 +0200

    Add documentation, dual camera.

--- pcd/scripts/ndtteapot.m
+++ pcd/scripts/ndtteapot.m
@@ -1,13 +1,38 @@
+% Visualizes the result of performing an NDT on the point cloud depicting a
+% teapot.
 
+% Define the resolution of the grid used for NDT.
 res = 0.2;
+
+% Define the radius used for NDT.
 radius = 1;
+
+% Read the point cloud.
 cloud = pcread('teapot.ply');
+
+% Perform NDT on the grid.
 xgv = cloud.XLimits(1)-0.5 : res : cloud.XLimits(2)+0.5;
 ygv = cloud.YLimits(1)-0.5 : res : cloud.YLimits(2)+0.5;
 zgv = cloud.ZLimits(1)-0.5 : res : cloud.ZLimits(2)+0.5;
 [x, y, z] = meshgrid(xgv, ygv, zgv);
 center = [x(:), y(:), z(:)];
 [mu, sigma] = ndt(cloud, center, radius);
+
+% Sum up the probabilty densities of all NDT distributions for all points 
+% of the grid.
 density = reshape(ndpdf(mu, sigma, center), size(x));
-isosurface(xgv, ygv, zgv, density, 170);
+
+% Plot a surface of constant density.
+ndtfig = figure('Name', 'Isosurface', 'NumberTitle', 'Off');
+isosurface(xgv, ygv, zgv, density, 100);
+ndtax = gca;
 axis equal
+
+% Plot the point cloud.
+cloudfig = figure('Name', 'Point cloud', 'NumberTitle', 'Off');
+cloudax = pcshow(cloud);
+
+% Link the cameras of the two figures together.
+cameralink = linkprop([ndtax, cloudax], {'CameraUpVector', ...
+    'CameraPosition', 'CameraTarget', 'CameraViewAngle'});
+setappdata(gcf, 'StoreTheLink', cameralink);
