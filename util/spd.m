3166f9ad4be7457aa595811b6fb92b8d967d8b18
Alexander Schaefer <aschaef@cs.uni-freiburg.de>
2016-06-21 13:49:41 +0200

    Add documentation.

--- util/spd.m
+++ util/spd.m
@@ -1,35 +1,51 @@
 function y = spd(x)
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
+% SPD Checks if matrix is symmetric positive definite.
+%   Y = SPD(X) checks whether the given matrix X is symmetric positive
+%   definite. If so, it is returned as Y. If not, Y is empty.
+%
+%   If X is close to singular, SPD changes the smallest eigenvalue of X so
+%   it is 0.001 of the largest eigenvalue. In this way, SPD ensures the
+%   numerical stability of the matrix.
+%
+%   Example:
+%      spd(zeros(3))
+%
+%   See also EIG.
 
+% Copyright 2016 Alexander Schaefer
+
+%% Create return value.
 y = [];
 
-% Check if matrix is symmetric.
+%% Check if matrix is symmetric.
 if size(x, 1) ~= size(x, 2)
     return
 end
 
-% Check if matrix values are finite.
-if any(any(~isfinite(x)))
+%% Check if matrix values are finite.
+if any(~isfinite(x(:)))
     return
 end
 
-% Check for positive definiteness.
+%% Check for positive definiteness.
+% The matrix is positive definite if all eigenvectors are positive.
 [eigenvector, eigenvalue] = eig(x);
 if any(diag(eigenvalue) <= 0)
     return
 end
 
-% If the matrix is close to singular, change it slightly to ensure
-% numerical stability of further computations.
+%% Ensure numerical stability.
+% Check if the matrix is close to singular.
 [mineig, minidx] = min(diag(eigenvalue));
 [maxeig, maxidx] = max(diag(eigenvalue));
 if mineig < maxeig * 1e-3
+    % Change the minimum eigenvector and reconstruct the matrix.
     eigenvalue(minidx,minidx) = eigenvalue(maxidx,maxidx) * 1e-3;
     y = eigenvector * eigenvalue / eigenvector;
     return
 end
 
+% If all checks passed, return the original matrix.
 y = x;
 
 end
