2b0d821d744f0ff82488e9550bff1ef82adb649d
Alexander Schaefer <aschaef@cs.uni-freiburg.de>
2016-06-21 15:20:05 +0200

    Fix visualization of NDT of teapot.

--- pcd/scripts/ndtteapot.m
+++ pcd/scripts/ndtteapot.m
@@ -1,11 +1,13 @@
-cloud = pcread('teapot.ply');
-res = 0.1;
-xgv = cloud.XLimits(1) : res : cloud.XLimits(2);
-ygv = cloud.YLimits(1) : res : cloud.YLimits(2);
-zgv = cloud.ZLimits(1) : res : cloud.ZLimits(2);
-[x, y, z] = ndgrid(xgv, ygv, zgv);
-center = [x(:), y(:), z(:)];
+
+res = 0.2;
 radius = 1;
+cloud = pcread('teapot.ply');
+xgv = cloud.XLimits(1)-0.5 : res : cloud.XLimits(2)+0.5;
+ygv = cloud.YLimits(1)-0.5 : res : cloud.YLimits(2)+0.5;
+zgv = cloud.ZLimits(1)-0.5 : res : cloud.ZLimits(2)+0.5;
+[x, y, z] = meshgrid(xgv, ygv, zgv);
+center = [x(:), y(:), z(:)];
 [mu, sigma] = ndt(cloud, center, radius);
-%density = pdf(gm, center);
-%isosurface(x(:), y(:), z(:), density, 0.5);
+density = reshape(ndpdf(mu, sigma, center), size(x));
+isosurface(xgv, ygv, zgv, density, 170);
+axis equal
