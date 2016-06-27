37a7b3d970207a132d609836924c5896bf348936
Alexander Schaefer <aschaef@cs.uni-freiburg.de>
2016-06-27 13:37:28 +0200

    Change alphaplot input arguments from volume limits to grid vectors.

--- plot/alphaplot.m
+++ plot/alphaplot.m
@@ -1,21 +1,25 @@
-function alphaplot(data, vol, varargin)
+function alphaplot(data, xgv, ygv, zgv, varargin)
 % ALPHAPLOT Visualize 3D array of scalars using semi-transparent voxels.
 %   ALPHAPLOT(DATA) takes the 3-dimensional array of scalar values DATA and
 %   creates a voxel plot. The element DATA(x,y,z) is represented by the 
 %   transparency of the cubic axis-aligned voxel with minimum limits 
-%   [x-1, y-1, z-1] and maximum limits [x, y, z]. 
+%   [x-1, y-1, z-1] and maximum limits [x, y, z].
 %   
 %   The values of DATA must stay within [0; 1]. DATA(x,y,z) == 0 
-%   corresponds to a completely transparent voxel; a value of 1 corresponds
-%   to a completely opaque voxel.
+%   corresponds to a transparent voxel; a value of 1 corresponds to an 
+%   opaque voxel.
 %
-%   ALPHAPLOT(DATA, VOL) divides the axis-aligned volume VOL into cuboid 
-%   voxels and uses this volume to visualize DATA. VOL is a 6-element 
-%   vector that contains the minimum and maximum limits of the volume: 
-%   [xmin, ymin, zmin, xmax, ymax, zmax].
+%   ALPHAPLOT(DATA, XGV, YGV, ZGV) shows no cubic, but cuboid voxels.
+%   XGV, YGV, ZGV are vectors that define the rasterization of the grid.
+%   A voxel with index [i, j, k] contains all points [x, y, z] that satisfy
+%   the inequality:
 %
-%   ALPHAPLOT(DATA, VOL, VARARGIN) plots voxels with the properties 
-%   indicated by the name-value pair arguments VARARGIN. 
+%         (XGV(i) <= x < XGV(i+1))
+%      && (YGV(j) <= y < YGV(j+1)) 
+%      && (ZGV(k) <= z < ZGV(k+1))
+%
+%   ALPHAPLOT(DATA, XGV, YGV, ZGV, VARARGIN) plots voxels with the 
+%   properties indicated by the name-value pair arguments VARARGIN. 
 %   For possible name-value pairs, see the documentation of PATCH.
 % 
 %   Example:
@@ -40,14 +44,21 @@ if min(data(:)) < 0 || max(data(:)) > 1
     error('X must be element of [0; 1].')
 end
 
-% If no volume limits are given, use cubic voxels with edge length 1.
+% If no volume limits are given, create them.
 if nargin < 2
-    vol = [0, 0, 0, size(data)];
+    xgv = 0 : size(data, 1);
+    ygv = 0 : size(data, 2);
+    zgv = 0 : size(data, 3);
 end
 
-% Check the volume limits.
-if any((vol(4:6) - vol(1:3)) <= 0)
-    error('Invalid argument VOL: VOL(1:3) >= VOL(4:6).')
+% Check whether the grid vectors contain enough elements.
+if min([numel(xgv), numel(ygv), numel(zgv)]) < 2
+    error('Every grid vector must contain at least 2 elements.')
+end
+
+% Check whether the grid vectors are ordered.
+if any(diff(xgv(:))<=0) || any(diff(ygv(:))<=0) || any(diff(zgv(:))<=0)
+    error('Grid vectors must monotonically increase.')
 end
 
 %% Compute voxel limits.
@@ -56,15 +67,9 @@ end
     1 : size(data, 1), 1 : size(data, 2), 1 : size(data, 3));
 i = [xmin(:), ymin(:), zmin(:)];
 
-% Compute the edge lengths of the voxels.
-edgelength = (vol(4:6)-vol(1:3)) ./ size(data);
-
 % Compute the minimum limits of the voxels.
-vox = repmat(vol(1:3), numel(xmin), 1) ...
-    + (i-1) .* repmat(edgelength, numel(xmin), 1);
-
-% Add the maximum limits.
-vox = [vox, vox + repmat(edgelength, size(vox, 1), 1)];
+vox = [xgv(i(:,1)); ygv(i(:,2)); zgv(i(:,3)); ...
+    xgv(i(:,1)+1); ygv(i(:,2)+1); zgv(i(:,3)+1)]';
 
 % Change the order of the data elements to the order of the voxels.
 faceAlpha = data(sub2ind(size(data), i(:,1), i(:,2), i(:,3)));
