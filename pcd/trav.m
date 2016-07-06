800597c323c54431879e6d38ab51ea223e21af14
Alexander Schaefer <aschaef@cs.uni-freiburg.de>
2016-07-06 15:15:21 +0200

    Replace old trav function by new vectorized version trav2.

--- pcd/trav.m
+++ pcd/trav.m
@@ -5,6 +5,8 @@ function [i, t] = trav(origin, ray, xgv, ygv, zgv)
 %   endpoint ORIGIN + RAY. The grid is defined by the grid vectors XGV, 
 %   YGV, and ZGV.
 %
+%   If the ray only touches the grid, no traversal is reported.
+%
 %   ORIGIN is a 3-element row vector that contains the coordinates of
 %   the starting point of the ray.
 %
@@ -22,8 +24,8 @@ function [i, t] = trav(origin, ray, xgv, ygv, zgv)
 %   I is an Mx3 matrix whose rows contain the x, y, and z indices of the
 %   voxels that the ray traverses, with M being the number of all voxels
 %   traversed. I is ordered: The first row corresponds to the voxel in 
-%   which the ray starts, the last row corresponds to the voxel where the 
-%   ray leaves the grid.
+%   which the ray starts or where it enters the grid, the last row 
+%   corresponds to the voxel where the ray ends or leaves the grid.
 %
 %   [I, T] = TRAV(ORIGIN, RAY, XGV, YGV, ZGV) also returns the M+1-element 
 %   column vector T. It contains the line parameters that encode the 
@@ -31,27 +33,16 @@ function [i, t] = trav(origin, ray, xgv, ygv, zgv)
 %   The first element corresponds to the entry point into the first voxel;
 %   in case the ray starts inside a voxel, it is 0. The transition point
 %   from the m-th to the m+1st voxel is computed ORIGIN + T(m+1)*RAY.
-%   ORIGIN + T(end)*RAY is the point where the ray leaves the volume or
-%   where it ends inside the grid.
+%   ORIGIN + T(end)*RAY is the point where the ray ends or where it leaves 
+%   the grid.
 %
-%   TRAV runs on the GPU if at least one input matrix is a gpuArray
-%   object. In this case, I is a Nx3 matrix and T is a Nx1 matrix, where
-%   N = numel(xgv) + numel(ygv) + numel(zgv). The last N-M rows of I and T 
-%   are NaN.   
-%
-%   Example (execution on CPU):
+%   Example:
 %      origin = [5, 1.5, 1];
 %      ray = [-10, 0, 0];
 %      gv = -2 : 2; xgv = gv; ygv = gv; zgv = gv;
-%      [i, t] = trav(origin, ray, xgv, ygv, zgv)
+%      [i, t] = trav2(origin, ray, xgv, ygv, zgv)
 %
-%   Example (execution on GPU):
-%      origin = gpuArray([5, 1.5, 1]);
-%      ray = gpuArray([-10, 0, 0]);
-%      gv = gpuArray(-2 : 2); xgv = gv; ygv = gv; zgv = gv;
-%      [i, t] = trav(origin, ray, xgv, ygv, zgv)
-%
-%   See also SLAB, GPUARRAY, NAN.
+%   See also SLAB, NAN.
 
 % Copyright 2016 Alexander Schaefer
 %
@@ -70,14 +61,14 @@ function [i, t] = trav(origin, ray, xgv, ygv, zgv)
 % Make sure the user specified enough input arguments.
 narginchk(5, 5);
 
+% Check if the input arguments are finite.
+if ~all(isfinite([origin(:); ray(:); xgv(:); ygv(:); zgv(:)]))
+    error('Input arguments must not be NaN or Inf.')
+end
+
 % Check if the arguments have the expected sizes.
 if numel(origin) ~= 3 || numel(ray) ~= 3
     error('ORIGIN and RAY must have 3 elements.')
-end
-
-% Check the ray vector.
-if ~all(isfinite(ray))
-    error('Ray values must not be NaN or Inf.')
 end
 
 % Check whether the grid vectors contain enough elements.
@@ -90,85 +81,52 @@ if any(diff(xgv(:))<=0) || any(diff(ygv(:))<=0) || any(diff(zgv(:))<=0)
     error('Grid vectors must monotonically increase.')
 end
 
-% Find out whether to execute on the CPU or on the GPU.
-gpu = isa(origin,'gpuArray') || isa(ray,'gpuArray') ...
-    || isa(xgv,'gpuArray') || isa(ygv,'gpuArray') || isa(zgv,'gpuArray');
-
 %% Initialization phase: calculate index of entry point.
-% Compute the size of the voxel grid.
-gridsize = [numel(xgv)-1, numel(ygv)-1, numel(zgv)-1];
-
 % Compute the intersections of the ray with the grid volume.
 vol = [xgv(1), ygv(1), zgv(1), xgv(end), ygv(end), zgv(end)];
 [hit, tvol] = slab(origin, ray, vol);
 
-% If the ray does not intersect with the volume, return immediately.
-if ~hit || tvol(2) < 0
-    i = []; 
-    t = [];
+% If the ray does not traverse the volume, return immediately.
+i = []; 
+t = [];
+if ~hit || tvol(2) < 0 || diff(tvol) == 0
     return
 end
 
-% Initialize return matrices. 
-i = NaN(sum(gridsize)+3, 3);
-t = NaN(sum(gridsize)+3, 1);
-if gpu
-    i = gpuArray(i);
-    t = gpuArray(t);
-end
-
-% Define the indices of the end of the payload data of the return matrices.
-iEnd = 0;
-tEnd = 0;
-
-% Compute the line parameter corresponding to the entry point into the 
-% grid.
-t(tEnd+1) = max([0, tvol(1)]);
-tEnd = tEnd + 1;
+% Compute the line parameter corresponding to the entry point of the ray
+% into the grid and the line parameter corresponding to the point where the
+% ray leaves the grid.
+tmin = max([0, tvol(1)]);
+tmax = min([1, tvol(2)]);
 
 % Calculate the index of the voxel corresponding to the starting point. 
-entry = origin + t(tEnd)*ray;
-iNext = [find(xgv(1:end-1) <= entry(1), 1, 'last'), ...
+entry = origin + tmin*ray;
+i = [find(xgv(1:end-1) <= entry(1), 1, 'last'), ...
     find(ygv(1:end-1) <= entry(2), 1, 'last'), ...
     find(zgv(1:end-1) <= entry(3), 1, 'last')];
 
 %% Incremental phase: calculate indices of traversed voxels.
-% Add voxels to the index matrix until the ray leaves the grid
-% or until the ray ends.
-while all(1 <= iNext & iNext <= gridsize) && t(tEnd) <= 1
-    % Add the index of the current voxel to the return matrix.
-    i(iEnd+1,:) = iNext;
-    iEnd = iEnd + 1;
-    
-    % Compute the bounds of the next voxel.
-    voxel = [xgv(i(iEnd,1)), ygv(i(iEnd,2)), zgv(i(iEnd,3));
-        xgv(i(iEnd,1)+1), ygv(i(iEnd,2)+1), zgv(i(iEnd,3)+1)];
+% Compute the line parameters of the ray corresponding to the intersections
+% with the grid planes.
+tx = (xgv(:) - origin(1)) / ray(1);
+ty = (ygv(:) - origin(2)) / ray(2);
+tz = (zgv(:) - origin(3)) / ray(3);
 
-    % Compute the line parameter of the intersection of the ray with the
-    % infinite planes that confine the next voxel.
-    tvox = (voxel - [origin; origin]) ./ [ray; ray];
+% Sort the intersections according to the line parameters.
+t = [unique(sort([0; 1; tx; ty; tz]))];
 
-    % Compute the line parameter of the intersection point of the ray with
-    % the joint face of the current and the next voxel.
-    tvox(repmat(any(isnan(tvox)), 2, 1)) = NaN;
-    tvox = max(tvox);
-    t(tEnd+1,1) = min(tvox);
-    tEnd = tEnd + 1;
+% Remove all intersections that lie outside the grid.
+t(t < tmin | t > tmax | ~isfinite(t)) = [];
 
-    % Determine the index step into the next voxel.
-    iStep = (tvox==t(tEnd)) .* sign(ray);
+% If the ray ends inside the grid, add the ray endpoint to the line
+% parameters.
+t([false(numel(t), 1); t(end) < 1 && tvol(2) > 1]) = 1;
 
-    % Compute the index of the next voxel.
-    iNext = i(iEnd,:) + iStep;
-end
+% For each line parameter compute the index step.
+iStep = [ismember(t, tx), ismember(t, ty), ismember(t, tz)] ...
+    .* repmat(sign(ray), size(t));
 
-% Make sure the line parameter never exceeds 1.
-t(tEnd) = min([1, t(tEnd)]);
-
-% If the function runs on the CPU, remove trailing NaN values.
-if ~gpu
-    i(isnan(i(:,1)),:) = [];
-    t(isnan(t)) = [];
-end
+% Sum up the index step to get the voxel indices.
+i = cumsum([i; iStep(2:end-1,:)], 1);
 
 end
