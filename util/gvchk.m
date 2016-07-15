0b19cdd503561002f29b3bc65ca11b40e0ad90eb
Alexander Schaefer <aschaef@cs.uni-freiburg.de>
2016-07-15 14:54:18 +0200

    Add check whether grid vector values are finite.

--- util/gvchk.m
+++ util/gvchk.m
@@ -2,7 +2,8 @@ function gvchk(varargin)
 % GVCHK Checks validity of grid vectors.
 %
 %   GVCHK(GVX, GVY, ...) checks whether each of the given grid vectors
-%   contains at least two elements and whether it increases monotonically.
+%   contains at least two elements, whether their values are finite and 
+%   whether they increase monotonically.
 %   If not, it displays an error message and stops the calling function.
 
 % Copyright 2016 Alexander Schaefer
@@ -12,6 +13,11 @@ for i = 1 : numel(varargin)
     % Check if the grid vector has at least 2 elements.
     if numel(varargin{i}) < 2
         error('Every grid vector must contain at least 2 elements.')
+    end
+    
+    % Check if the grid vector values are finite.
+    if any(~isfinite(varargin{i}(:)))
+        error('Grid vector elements must not be NaN or Inf.')
     end
     
     % Check whether the grid vector is ordered.
