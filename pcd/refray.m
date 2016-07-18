01b1bce56c43f39c73f05d01c90a0ffd3e4047de
Alexander Schaefer <fudger@users.noreply.github.com>
2016-07-18 22:12:23 +0200

    Add case differentiation for NaN and real-numbered measurements.

--- pcd/refray.m
+++ pcd/refray.m
@@ -34,7 +34,7 @@ function p = refray(origin, ray, rlim, ref, xgv, ygv, zgv)
 %      ray = [3, 4, 5];
 %      ref = repmat(magic(5)/100, [1, 1, 5]);
 %      gv = 0 : 5; 
-%      p = refray(origin, ray, lim, ref, gv, gv, gv)
+%      p = refray(origin, ray, [1, 10], ref, gv, gv, gv)
 %
 %   See also REFMAP, DECAYRAY.
 
@@ -77,17 +77,21 @@ if any(size(ref) ~= [numel(xgv)-1, numel(ygv)-1, numel(zgv)-1])
     error('Size of REF does not match grid vectors.')
 end
 
+%% Preprocess input arguments.
+% Compute ray lengths.
+l = sqrt(sum(ray.^2, 2));
+
+% Determine which rays are no-returns.
+inan = l < rlim(1) | l > rlim(2);
+
+% Set the length of no-return rays to maximum sensor range.
+rmax = rlim(2);
+ray(inan,:) = (ray(inan,:) ./ repmat(l(inan), 1, 3)) * rmax;
+
 %% Compute probability of measurements.
-% Determine the number of rays.
-nray = size(origin, 1);
-
-% Preallocate the return matrix.
-p = zeros(nray, 1);
-
-l = norm(ray);
-inan = find(l < rlim(1) | l > rlim(2));
-
 % Loop over all rays.
+nray = size(origin, 1);
+p = zeros(nray, 1);
 parfor i = 1 : nray
     % Compute the indices of the grid cells that the ray traverses.
     [vi, t] = trav(origin(i,:), ray(i,:), xgv, ygv, zgv);
@@ -95,26 +99,31 @@ parfor i = 1 : nray
     % Convert the subscript indices to linear indices.
     vi = sub2ind(size(ref), vi(:,1), vi(:,2), vi(:,3));
     
-    % Compute the indices of the voxels on the ray that are next to the 
-    % minimum and maximum measurement range barrier.
-    [~, ilim] = min(abs(...
-        repmat(t*norm(ray), 1, numel(rlim)) - repmat(rlim, numel(t), 1)));
-    ilim = ilim - 1;
+    % Compute the probability depending on whether or not the ray returned.
+    if ismember(i, inan)
+        % Compute the indices of the voxels on the ray next to the minimum 
+        % and maximum measurement range barrier.
+        [~, ilim] = min(abs(...
+            repmat(t*rmax, 1, numel(rlim)) - repmat(rlim, numel(t), 1)));
+        ilim = ilim - 1;
+        
+        % Calculate the probability that the ray is reflected before 
+        % reaching the minimum sensor range.
+        isub = vi(1 : ilim(1));
+        psub = 1 - prod(1-ref(isub));
     
-    % Calculate the probability that the ray is reflected before reaching
-    % the minimum sensor range.
-    isub = vi(1 : ilim(1));
-    msub = ref(isub);
-    psub = 1 - prod(1-msub);
+        % Calculate the probability that the ray surpasses the maximum 
+        % sensor range.
+        isup = vi(ilim(1)+1 : ilim(2));
+        psup = prod(1-ref(isup));
     
-    % Calculate the probability that the ray surpasses the maximum sensor 
-    % range.
-    isup = vi(ilim(1)+1 : ilim(2));
-    msup = ref(isup);
-    psup = prod(1-msup);
-    
-    % Sum up the probabilities.
-    p(i) = psub + psup;
+        % Sum up the probabilities to get the probability of NaN.
+        p(i) = psub + psup;
+    else      
+        % Compute the probability of the given measurement.
+        m = ref(vi);
+        p(i) = m(end) * prod(1 - m(1:end-1));
+    end
 end
 
 end
