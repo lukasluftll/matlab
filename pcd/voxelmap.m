685eadee31cdb7298e272c67b40c8030a83ee9e6
Alexander Schaefer <aschaef@cs.uni-freiburg.de>
2016-07-20 11:35:59 +0200

    Add plotting function.

--- pcd/voxelmap.m
+++ pcd/voxelmap.m
@@ -30,11 +30,90 @@ classdef voxelmap
     
     % Copyright 2016 Alexander Schaefer
     
-    properties
+    properties ( SetAccess = private )
+        data;
+        xgv;
+        ygv;
+        zgv;
     end
     
-    methods
+    methods ( Access = public )
+        % Construct a voxelmap object.
+        function obj = voxelmap(data, xgv, ygv, zgv)
+            % Check number of input arguments.
+            if ~(nargin == 1 || nargin == 4)
+                error('VOXELMAP takes 1 or 4 input arguments.')
+            end
+            
+            % If the grid vectors are not given, construct them.
+            if nargin == 1
+                xgv = 0 : size(data, 1);
+                ygv = 0 : size(data, 2);
+                zgv = 0 : size(data, 3);
+            end
+            
+            % Check the grid vectors.
+            gvchk(xgv, ygv, zgv);
+            
+            % Store the input.
+            obj.data = data;
+            obj.xgv = xgv;
+            obj.ygv = ygv;
+            obj.zgv = zgv;
+        end
+
+        % Plot the map.
+        function plot(obj, mode)
+            % PLOT Plot voxel map.
+            %   PLOT(VM, MODE) visualizes the voxelmap object VM using
+            %   semi-transparent voxels. 
+            %
+            %   MODE can assume the following values:
+            %
+            %      'scale' (default) - The highest value of VM is drawn as
+            %                          completely intransparent voxel, the 
+            %                          lowest value of VM is drawn as
+            %                          completely transparent voxel.
+            %      'direct'          - Data values are directly interpreted 
+            %                          as transparencies, with 0
+            %                          representing complete intransparency
+            %                          and 1 representing complete
+            %                          transparency.
+            
+            %% Validate input.
+            % Check the number of input arguments.
+            narginchk(1, 2);
+            
+            % If there is mode given, set it.
+            if nargin < 2
+                mode = 'scale';
+            end
+            
+            %% Plot map.
+            switch lower(mode)
+                case 'scale'
+                    % Compute minimum and maximum data elements.
+                    lim = [min(obj.data(:)), max(obj.data(:))];
+                    
+                    % Compute the transparency values to plot.
+                    t = (obj.data + lim(1)) * diff(lim);
+                case 'direct'
+                    % Crop data to interval [0; 1].
+                    t = obj.data;
+                    t(t < 0) = 0;
+                    t(t > 1) = 1;
+                otherwise
+                    error(['MODE ', mode, ' not supported.'])
+            end
+            
+            % Plot.
+            alphaplot(t, obj.xgv, obj.ygv, obj.zgv);
+            axis equal
+            grid on
+            xlabel('x')
+            ylabel('y')
+            zlabel('z')
+        end
     end
-    
 end
 
