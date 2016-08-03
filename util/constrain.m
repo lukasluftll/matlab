cc097b262948d3bf21b80df4fa3bd9d4ee4e8c10
Alexander Schaefer <aschaef@cs.uni-freiburg.de>
2016-08-03 17:25:05 +0200

    Add function to constrain values to interval.

---
+++ util/constrain.m
@@ -0,0 +1,30 @@
+function y = constrain(x, lim)
+% CONSTRAIN Fit value into interval.
+%   Y = CONSTRAIN(X, LIM) fits all values of matrix X into the interval
+%   defined by the ordered 2-element vector LIM.
+%
+%   Example:
+%      constrain(1:9, [3,6])
+%
+%   See also MIN, MAX.
+
+% Copyright 2016 Alexander Schaefer
+
+%% Validate input.
+% Check the number of input arguments.
+narginchk(2, 2)
+
+% Make sure the interval is an ordered 2-element vector.
+if numel(lim) ~= 2
+    error('LIM must have exactly 2 elements.')
+end
+if diff(lim <= 0)
+    error('LIM must be ordered.')
+end
+
+%% Fit X into LIM.
+x(x < lim(1)) = lim(1);
+x(x > lim(2)) = lim(2);
+y = x;
+
+end
