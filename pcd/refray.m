9b0b7fdb37c5cbb8ff9e8934dcbcbb990f3471da
Alexander Schaefer <aschaef@cs.uni-freiburg.de>
2016-08-04 16:31:14 +0200

    Prepare refray to take affine3d transformations as input.

--- pcd/refray.m
+++ pcd/refray.m
@@ -1,12 +1,14 @@
-function p = refray(origin, ray, rlim, ref)
+function p = refray(mts, ray, rlim, ref)
 % REFRAY Compute probability of Lidar measurement from reflectivity map.
-%   P = REFRAY(ORIGIN, RAY, RLIM, REF) computes the probability of 
-%   obtaining the Lidar measurement defined by ORIGIN and RAY conditioned 
-%   on the reflectivity map REF.
+%   P = REFRAY(MTS, RAY, RLIM, REF) computes the probability of obtaining 
+%   the Lidar measurement defined by ORIGIN and RAY conditioned on the 
+%   reflectivity map REF.
 %
-%   ORIGIN and RAY are Mx3 matrices whose rows contain the Cartesian 
-%   origin coordinates and the ray vectors of the M measured rays. If all 
-%   rays originate from the same point, ORIGIN may also be a 1x3 matrix.
+%   MTS is an affine3d object that defines the pose of the sensor with
+%   respect to the reflectivity map frame.
+%
+%   RAY is a Mx3 matrix whose rows contain the ray direction vectors of the 
+%   M measured rays in the sensor frame.
 %
 %   RLIM is a 2-element vector that defines the minimum and the maximum
 %   radius detected by the Lidar sensor. RAY rows whose lengths are not 
@@ -21,13 +23,12 @@ function p = refray(origin, ray, rlim, ref)
 %   corresponds to the probability of obtaining the m-th measurement.
 %
 %   Example:
-%      origin = [0, 0, 0];
 %      ray = [3, 4, 5];
 %      rlim = [1, 10];
 %      ref = voxelmap(repmat(magic(5)/100, [1, 1, 5]), 0:5, 0:5, 0:5);
-%      p = refray(origin, ray, rlim, ref)
+%      p = refray(affine3d(), ray, rlim, ref)
 %
-%   See also VOXELMAP, REFMAP, DECAYRAY.
+%   See also AFFINE3D, VOXELMAP, REFMAP, DECAYRAY.
 
 % Copyright 2016 Alexander Schaefer
 
@@ -35,23 +36,21 @@ function p = refray(origin, ray, rlim, ref)
 % Check whether the user provided the correct number of input arguments.
 narginchk(4, 4)
 
-% Check if the arguments have the expected numbers of dimensions.
-if ~ismatrix(origin) || ~ismatrix(ray)
-    error('ORIGIN and RAY must be 2D matrices.')
+% Check the sensor pose.
+if ~isa(mts, 'affine3d')
+    error('MTS must be an affine3d object.')
 end
 
-% Check if the arguments have the expected sizes.
-if size(origin, 2) ~= 3 || size(ray, 2) ~= 3
-    error('ORIGIN and RAY must have 3 columns.')
+% Check the ray matrix.
+if ~ismatrix(ray)
+    error('RAY must be a 2D matrix.')
 end
-
-% If ORIGIN has only one row, expand it to match the row size of RAY.
-if size(origin, 1) == 1
-    origin = repmat(origin, size(ray, 1), 1);
+if size(ray, 2) ~= 3
+    error('RAY must have 3 columns.')
 end
 
 % Make sure all input arguments contain finite values only.
-if ~all(isfinite([origin(:); ray(:); rlim(:); ref.data(:)]))
+if ~all(isfinite([ray(:); rlim(:); ref.data(:)]))
     error('Input arguments must not be NaN or Inf.')
 end
 
@@ -72,13 +71,16 @@ inan = l < rlim(1) | l > rlim(2);
 rnan = rlim(2) + sqrt(3)*max([diff(ref.xgv),diff(ref.ygv),diff(ref.zgv)]);
 ray(inan,:) = ray(inan,:) ./ [zeros(0, 3); repmat(l(inan), 1, 3)] * rnan;
 
+% Transform rays from the sensor frame to the map frame.
+
+
 %% Compute probability of measurements.
 % Loop over all rays.
 nray = size(ray, 1);
 p = zeros(nray, 1);
 parfor i = 1 : nray
     % Compute the indices of the grid cells that the ray traverses.
-    [vi, t] = trav(origin(i,:), ray(i,:), ...
+    [vi, t] = trav(mts(i,:), ray(i,:), ...
         ref.xgv, ref.ygv, ref.zgv); %#ok<PFBNS>
     
     % Convert the subscript indices to linear indices.
