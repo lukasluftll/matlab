636ee97e014eff08f991fd66d20b67b40fe67842
Alexander Schaefer <aschaef@cs.uni-freiburg.de>
2016-08-08 11:59:08 +0200

    Adapt refray to take laserscan objects as input.

--- pcd/refray.m
+++ pcd/refray.m
@@ -14,7 +14,7 @@ function p = refray(ls, ref)
 %
 %   Example:
 %      pcd = pcdread('castle.pcd');
-%      ls = laserscan(pcd.azimuth,pcd.elevation,pcd.radius,eye(4),[1,100]);
+%      ls = laserscan(pcd.azimuth, pcd.elevation, pcd.radius, [1,100]);
 %      ref = refmap(ls, 0:5, 0:5, 0:5);
 %      p = refray(ls, ref)
 %
@@ -35,14 +35,13 @@ if ~isa(ref, 'voxelmap')
 end
 
 %% Preprocess input arguments.
+% Convert the ray coordinates from spherical to Cartesian.
+ray = cart(ls);
+
 % Set the length of no-return rays to maximum sensor range plus the
 % diameter of the largest voxel.
-rnan = rlim(2) + sqrt(3)*max([diff(ref.xgv),diff(ref.ygv),diff(ref.zgv)]);
-radius = ls.radius;
-radius(noret(ls)) = rnan;
-
-% Transform rays from the sensor frame to the map frame.
-
+d = sqrt(3) * max([diff(ref.xgv), diff(ref.ygv), diff(ref.zgv)]);
+ray(~ret(ls),:) = ray(~ret(ls),:) * (ls.rlim(2) + d);
 
 %% Compute probability of measurements.
 % Loop over all rays.
@@ -50,19 +49,22 @@ nray = size(ray, 1);
 p = zeros(nray, 1);
 parfor i = 1 : nray
     % Compute the indices of the grid cells that the ray traverses.
-    [vi, t] = trav(mts(i,:), ray(i,:), ...
-        ref.xgv, ref.ygv, ref.zgv); %#ok<PFBNS>
+    [vi,t] = trav(ls.pos, ray(i,:), ref.xgv, ref.ygv, ref.zgv); %#ok<PFBNS>
     
-    % Convert the subscript indices to linear indices.
+    % Convert the subscript voxel indices to linear indices.
     vi = sub2ind(size(ref.data), vi(:,1), vi(:,2), vi(:,3));
     
     % Compute the measurement probability depending on whether or not the 
     % ray returned.
-    if inan(i) % Ray does not return.
+    if ret(i) % Ray returns.
+        % Compute the probability of the ray being reflected in the last
+        % voxel it traverses.
+        p(i) = ref.data(vi(end)) * prod(1 - ref.data(vi(1:end-1)));
+    else % Ray does not return.
         % Compute the indices of the voxels on the ray directly in front
         % of the beginning and in front of the end of the measurement 
         % interval.
-        ilim = knnsearch(t*rnan, rlim(:)) - 1; %#ok<PFBNS>
+        ilim = knnsearch(t*nrray, ls.rlim(:)) - 1;
         
         % Calculate the probability that the ray is reflected before 
         % reaching the minimum sensor range.
@@ -77,10 +79,6 @@ parfor i = 1 : nray
         % Sum up the probabilities to get the probability of the ray being
         % reflected before or after the measurement interval.
         p(i) = psub + psup;
-    else % Ray returns.
-        % Compute the probability of the ray being reflected in the last
-        % voxel it traverses.
-        p(i) = ref.data(vi(end)) * prod(1 - ref.data(vi(1:end-1)));
     end
 end
 
