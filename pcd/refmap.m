adeff721a9dc3d5639e8b432adcc1e773a1a082f
Alexander Schaefer <aschaef@cs.uni-freiburg.de>
2016-08-08 13:38:08 +0200

    Adapt refmap to take laserscan object as input.

--- pcd/refmap.m
+++ pcd/refmap.m
@@ -1,23 +1,11 @@
-function [ref, h, m] = refmap(mts, azi, ele, r, ret, xgv, ygv, zgv)
-% REFMAP Compute reflectivity map from Lidar rays in grid volume.
-%   REF = REFMAP(MTS, AZI, ELE, R, RET, XGV, YGV, ZGV) uses the rays 
-%   represented in spherical coordinates azimuth AZI, elevation ELE, and 
-%   radius R to compute the reflectivity of each voxel in the grid volume 
-%   defined by the grid vectors XGV, YGV, ZGV.
+function [ref, h, m] = refmap(ls, xgv, ygv, zgv)
+% REFMAP Compute reflectivity map from laser scan in grid volume.
+%   REF = REFMAP(LS, XGV, YGV, ZGV) uses the laser scan LS to compute the 
+%   reflectivity of each voxel in the grid volume defined by the grid 
+%   vectors XGV, YGV, ZGV.
 %
-%   MTS is an affine3d object that defines the pose of the sensor with
-%   respect to the reflectivity map frame.
-%
-%   AZI and ELE are HEIGHTxWIDTH matrices that define the azimuth and 
-%   elevation angles of the laser rays with respect to the sensor frame.
-%   HEIGHT and WIDTH describe the size of the point cloud. The unit is rad.
-%
-%   R is a HEIGHTxWIDTH matrix that contains the length of the respective 
-%   ray. For no-return rays, this length must equal the maximum sensor 
-%   range.
-%
-%   RET is a HEIGHTxWIDTH logical matrix that indicates whether or not the
-%   respective ray was reflected within the sensor range. 
+%   LS is a laserscan object. The sensor pose of the scan is assumed to be 
+%   specified with respect to the reflectivity map frame.
 %
 %   XGV, YGV, ZGV are vectors that define the rasterization of the grid.
 %   A voxel with index [i, j, k] contains all points [x, y, z] that satisfy
@@ -33,21 +21,18 @@ function [ref, h, m] = refmap(mts, azi, ele, r, ret, xgv, ygv, zgv)
 %   the voxel. If the voxel has not been visited by any ray, its 
 %   reflectivity is NaN.
 %
-%   [REF, H, M] = REFMAP(MTS, AZI, ELE, R, RET, XGV, YGV, ZGV) also returns
-%   the voxelmap objects H and M. 
+%   [REF, H, M] = REFMAP(LS, XGV, YGV, ZGV) also returns the voxelmap 
+%   objects H and M. 
 %   H contains the number of ray remissions for each voxel. 
 %   M contains for each voxel the number of rays that traversed the voxel
 %   without being reflected.
 %
 %   Example:
-%      pc = pcdread('castle.pcd');
-%      radiusFinite = pc.radius; radiusFinite(isnan(radiusFinite)) = 130;
-%      hgv = -100 : 5 : 100;
-%      vgv = -20 : 5 : 20;
-%      ref = refmap(affine3d(), pc.azimuth, pc.elevation, radiusFinite, ...
-%                   isfinite(pc.radius), hgv, hgv, vgv)
+%      pcd = pcdread('castle.pcd');
+%      ls = laserscan(pcd.azimuth, pcd.elevation, pcd.radius, [1, 100]);
+%      ref = refmap(ls, -100:5:100, -100:5:100, -20:5:20)
 %
-%   See also AFFINE3D, VOXELMAP, REFRAY, REFNANRAY, DECAYMAP.
+%   See also LASERSCAN, VOXELMAP, REFRAY, DECAYMAP.
 
 % Copyright 2016 Alexander Schaefer
 %
@@ -58,40 +43,32 @@ function [ref, h, m] = refmap(mts, azi, ele, r, ret, xgv, ygv, zgv)
 
 %% Validate input.
 % Check number of input arguments.
-narginchk(8, 8);
+narginchk(4, 4);
 
-% Check the sensor pose.
-if ~isa(mts, 'affine3d')
-    error('MTS must be an affine3d object.')
-end
-
-% Check whether the spherical coordinate matrices and the reflection matrix
-% all have the same number of dimensions and the same size.
-if ~(ismatrix(azi) && ismatrix(ele) && ismatrix(r) && ismatrix(ret))
-    error('AZI, ELE, R, and RET must be 2D matrices.')
-end
-if any(size(azi)~=size(ele) | size(azi)~=size(r) | size(azi)~=size(ret))
-    error('AZI, ELE, R, and RET must have the same size.')
-end
-
-% Make sure all input arguments are finite.
-if ~all(isfinite([azi(:); ele(:); r(:); ret(:); xgv(:); ygv(:); zgv(:)]))
-    error('Input arguments must not be NaN or Inf.')
+% Check the laserscan.
+if ~isa(ls, 'laserscan')
+    error('LS must be a laserscan object.')
 end
 
 % Check the grid vectors.
 gvchk(xgv, ygv, zgv);
 
+% If the sensor measurement range starts at a positive value, issue a
+% warning.
+if ls.rlim(1) > 0
+    warning(['LS.RLIM(1) > 0, but all no-return ray lengths are ', ...
+      'assumed to surpass LS.RLIM(2), not to fall into [0; LS.RLIM(1)].'])
+end
+
 %% Preprocess input data.
 % Compute the Cartesian ray direction vectors.
-[dirx, diry, dirz] = sph2cart(azi, ele, r);
+ray = cart(ls);
 
-% Rotate the ray direction vectors according to the sensor orientation.
-rot = affine3d([mts.T(1:3,:); 0, 0, 0, 1]);
-[dirx, diry, dirz] = transformPointsForward(rot, dirx, diry, dirz);
+% Compute the indices of the returned rays.
+iret = ret(ls);
 
-% Determine the number of rays.
-nray = numel(azi);
+% Set the length of no-return rays to maximum sensor range.
+ray(~iret,:) = ray(~iret,:) * ls.rlim(2);
 
 % Determine the size of the voxel grid.
 gridsize = [numel(xgv), numel(ygv), numel(zgv)] - 1;
@@ -107,14 +84,13 @@ spmd
     % Loop over the worker's share of all rays.
     for i = labindex : numlabs : nray
         % Compute the indices of the voxels through which the ray travels.
-        trans = mts.T(4,1:3);
-        [vi, t] = trav(trans, [dirx(i), diry(i), dirz(i)], xgv, ygv, zgv);
+        [vi, t] = trav(pos(ls), ray(i,:), xgv, ygv, zgv);
 
         % Convert the subscript indices to linear indices.
         vi = sub2ind(gridsize, vi(:,1), vi(:,2), vi(:,3));
 
         % For each voxel, sum up the number of hits and misses.
-        hw(vi(end)) = hw(vi(end)) + (ret(i) && t(end)==1);
+        hw(vi(end)) = hw(vi(end)) + (iret(i) && t(end)==1);
         mw(vi) = mw(vi) + t(2:end)<1;
     end
 end
