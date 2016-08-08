804f91bd86982e8221e0e5361480c247894a43c5
Alexander Schaefer <fudger@users.noreply.github.com>
2016-08-08 09:58:23 +0200

    Finish implementation and documentation.

--- pcd/laserscan.m
+++ pcd/laserscan.m
@@ -1,31 +1,25 @@
 classdef laserscan < handle
-    % LASERSCAN Object for storing a 3D laser scan.
-    %   L = LASERSCAN(AZIMUTH, ELEVATION, RADIUS) creates a laser scan
-    %   object.
+    % LASERSCAN Object for storing 3D laser scan.
+    %   L = LASERSCAN(AZIMUTH, ELEVATION, RADIUS, RLIM, TFORM) creates a 
+    %   laser scan object.
     %
-    %   AZIMUTH, ELEVATION, and RADIUS are HEIGHTxWIDTH matrices which
-    %   define the rays the laser sensor measured in spherical coordinates.
+    %   AZIMUTH, ELEVATION, and RADIUS are HEIGHTxWIDTH matrices defining
+    %   the rays the laser sensor measured in spherical coordinates w.r.t.
+    %   the sensor frame.
     %   AZIMUTH and ELEVATION must be finite angles in [rad].
-    %   RADIUS may contain infinite values, which are interpreted as 
-    %   no-return rays.
+    %   RADIUS may contain real and infinite values. All RADIUS values 
+    %   outside interval RLIM are interpreted as no-return rays.
     %
-    %   The laser sensor pose is assumed to be equal to the reference 
-    %   frame.
+    %   RLIM is a 2-element vector that defines the minimum and maximum 
+    %   radius the sensor is able to measure.
     %
-    %   L = LASERSCAN(AZIMUTH, ELEVATION, RADIUS, TFORM) additionally
-    %   specifies the laser sensor pose with respect to the global 
-    %   reference frame.
-    %
-    %   TFORM is a homogeneous 4x4 transformation matrix defining rotation 
-    %   and translation only, no sheering, scaling, etc.
-    %
-    %   L = LASERSCAN(AZIMUTH, ELEVATION, RADIUS, TFORM, RLIM) additionally
-    %   defines the minimum and maximum radius the sensor is able to 
-    %   measure using the ordered 2-element vector RLIM.
+    %   TFORM is a homogeneous 4x4 transformation matrix defining the 
+    %   transformation from a global reference frame to the laser sensor 
+    %   frame. It defaults to identity.
     %
     %   Example:
     %      pcd = pcdread('castle.pcd');
-    %      l = laserscan(pcd.azimuth, pcd.elevation, pcd.radius)
+    %      l = laserscan(pcd.azimuth, pcd.elevation, pcd.radius, [0, 100])
     %
     %   See also TRAV, SLAB.
     
@@ -42,7 +36,12 @@ classdef laserscan < handle
     methods
         % Change the azimuth angles.
         function set.azimuth(obj, azimuth)
-            % Check if AZIMUTH has the same size as the current azimuth 
+            % Check if AZIMUTH is a 2D matrix.
+            if ~ismatrix(azimuth)
+                error('AZIMUTH must be a 2D matrix.')
+            end
+            
+            % Check if AZIMUTH has the same size as the current azimuth
             % data.
             if ~isempty(obj.azimuth) && size(azimuth) ~= size(obj.azimuth)
                 error(['AZIMUTH must be a matrix of size ', ...
@@ -61,6 +60,11 @@ classdef laserscan < handle
         
         % Change the elevation angles.
         function set.elevation(obj, elevation)
+            % Check if ELEVATION is a 2D matrix.
+            if ~ismatrix(elevation)
+                error('ELEVATION must be a 2D matrix.')
+            end
+            
             % Check if ELEVATION has the same size as the current elevation
             % data.
             if ~isempty(obj.elevation) ...
@@ -81,6 +85,11 @@ classdef laserscan < handle
         
         % Change the ray radii.
         function set.radius(obj, radius)
+            % Check if RADIUS is a 2D matrix.
+            if ~ismatrix(radius)
+                error('RADIUS must be a 2D matrix.')
+            end
+            
             % Check if RADIUS has the same size as the current radius data.
             if ~isempty(obj.radius) && size(radius) ~= size(obj.radius)
                 error(['RADIUS must be a matrix of size ', ...
@@ -97,6 +106,7 @@ classdef laserscan < handle
         
         % Change the sensor pose.
         function set.tform(obj, tform)
+            % Check if TFORM is a homogeneous transformation matrix.
             if ~ishrt(tform)
                 error(['TFORM must be a homogeneous ', ...
                     '4x4 rotation-translation matrix.'])
@@ -118,6 +128,11 @@ classdef laserscan < handle
                 error('RLIM(1) must be greater or equal zero.')
             end
             
+            % Check the all values are finite.
+            if ~all(isfinite(rlim))
+                error('RLIM must be finite.')
+            end
+            
             % Check the limit vector is ordered.
             if diff(rlim) <= 0
                 error('RLIM(2) must be greater than RLIM(1).');
@@ -129,19 +144,14 @@ classdef laserscan < handle
     end
     
     methods ( Access = public )
-        % Construct a laserscan object.
-        function obj = laserscan(azimuth, elevation, radius, tform, rlim)
+        % Construct laserscan object.
+        function obj = laserscan(azimuth, elevation, radius, rlim, tform)
             % Check number of input arguments.
-            narginchk(3, 5);
-
-            % If the sensor pose is not given, set it to identity.
-            if nargin < 4
-                tform = eye(4);
-            end
+            narginchk(4, 5);
             
-            % If the limit vector is not given, define it.
+            % If the sensor pose is not given, set it to identity.
             if nargin < 5
-                rlim = [0; +Inf];
+                tform = eye(4);
             end
             
             % Store the input.
@@ -152,20 +162,20 @@ classdef laserscan < handle
             obj.rlim = rlim;
         end
         
-        % Identify the no-return rays.
-        function inr = noret(obj)
-            % NORET Identify no-return rays.
-            %   INR = NORET(OBJ) returns the subscript indices of the
-            %   no-return rays of the laser scan.
+        % Identify the return and no-return rays.
+        function r = ret(obj)
+            % RET Identify return and no-return rays.
+            %   R = RET(OBJ) returns for each ray of the laser scan 
+            %   whether it was reflected by the environment or not.
             %
-            %   INR is a HEIGHTxWIDTH logical matrix, where HEIGHT and
-            %   WIDTH define the size of the spherical coordinate matrices
+            %   R is a HEIGHTxWIDTH logical matrix, where HEIGHT and WIDTH 
+            %   define the size of the spherical coordinate matrices
             %   AZIMUTH, ELEVATION, and RADIUS used when constructing the
-            %   laserscan object. All elements of INR that correspond to
-            %   no-return rays are true, all others are false.
+            %   laserscan object. All true elements of IR correspond to
+            %   returned rays, all false elements correspond to no-return
+            %   rays.
             
-            inr = ~isfinite(obj.radius) | obj.radius < obj.rlim(1) ...
-                | obj.radius > obj.rlim(2);
+            r = obj.radius >= obj.rlim(1) & obj.radius <= obj.rlim(2);
         end
         
         % Plot the laser scan.
@@ -174,62 +184,62 @@ classdef laserscan < handle
             %   PLOT(OBJ) visualizes the rays originating from the laser 
             %   scanner. Returned rays are plotted in red, no-return rays 
             %   are plotted in light gray.
-                        
-            %% Compute visualized radius of no-return rays.
-            plotradius = obj.radius;
-            if isfinite(obj.rlim(2))
-                plotradius(noret(obj)) = obj.rlim(2);
-            else
-                plotradius(noret(obj)) = max(obj.radius);    
-            end
 
+            %% Convert spherical to Cartesian coordinates.
+            % Define the plotted length of all rays, also no-returns.
+            plotradius = obj.radius;
+            plotradius(~ret(obj)) = obj.rlim(2);
+            
             % Convert the spherical coordinates to Cartesian coordinates.
             [x, y, z] = sph2cart(obj.azimuth, obj.elevation, plotradius);
             
             % Rotate the rays around the origin.
-            
-            
-            p = [x, y, z];
+            p = zeros(numel(x), 3);
+            for i = 1 : numel(x)
+                p(i,:) = (obj.tform(1:3,1:3) * [x(i); y(i); z(i)])';
+            end
 
-%% Plot rays.
-% Plot the returned rays.
-pret = kron(p(ret(:),:), [0; 1]);
-retray = plot3(pret(:,1), pret(:,2), pret(:,3), 'Color', 'red');
-if ~isempty(retray)
-    retray.Color(4) = 0.5;
-end
+            %% Plot rays.
+            % Plot the returned rays.
+            r = ret(obj);
+            pr = kron(p(r(:),:), [0; 1]);
+            pr = pr + repmat(obj.tform(1:3,4)', size(pr, 1), 1);
+            rray = plot3(pr(:,1), pr(:,2), pr(:,3), 'Color', 'red');
+            if ~isempty(rray)
+                % Set transparency.
+                rray.Color(4) = 0.5;
+            end
 
-% Plot the no-return rays.
-pnan = kron(p(~ret(:),:), [0; 1]);
-hold on
-nanray = plot3(pnan(:,1), pnan(:,2), pnan(:,3), 'Color', 'k');
-if ~isempty(nanray)
-    nanray.Color(4) = 0.03;
-end
+            % Plot the no-return rays.
+            pnr = kron(p(~r(:),:), [0; 1]);
+            pnr = pnr + repmat(obj.tform(1:3,4)', size(pnr, 1), 1);
+            hold on
+            nrray = plot3(pnr(:,1), pnr(:,2), pnr(:,3), 'Color', 'k');
+            if ~isempty(nrray)
+                % Set transparency.
+                nrray.Color(4) = 0.03;
+            end
+            hold off
 
-%% Plot decoration.
-% Plot the sensor origin.
-plot3(0, 0, 0, 'Color', 'k', 'Marker', '.', 'MarkerSize', 50);
+            %% Plot decoration.
+            % Plot the sensor origin.
+            plot3(0, 0, 0, 'Color', 'k', 'Marker', '.', 'MarkerSize', 50);
 
-% Plot the Cartesian axes.
-plot3([0, max(x(:))], [0, 0], [0, 0], 'Color', 'r', 'LineWidth', 3);
-plot3([0, 0], [0, max(y(:))], [0, 0], 'Color', 'g', 'LineWidth', 3);
-plot3([0, 0], [0, 0], [0, max(z(:))], 'Color', 'b', 'LineWidth', 3);
+            % Plot the Cartesian axes.
+            plot3([0, max(x(:))], [0, 0], [0, 0], 'Color', 'r', ...
+                'LineWidth', 3);
+            plot3([0, 0], [0, max(y(:))], [0, 0], 'Color', 'g', ...
+                'LineWidth', 3);
+            plot3([0, 0], [0, 0], [0, max(z(:))], 'Color', 'b', ...
+                'LineWidth', 3);
 
-% Plot the point cloud.
-pcshow(pointCloud(pret), 'MarkerSize', 30);
-hold off
+            % Label the axes.
+            xlabel('x')
+            ylabel('y')
+            zlabel('z')
 
-% Label the axes.
-xlabel('x')
-ylabel('y')
-zlabel('z')
-
-axis equal
-grid on
-
-end
-
+            axis equal
+            grid on
         end
-    end   
+    end
 end
