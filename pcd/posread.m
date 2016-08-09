5eaa1aaa3adb96d573ca61d9e18b1abe2439ad76
Alexander Schaefer <aschaef@cs.uni-freiburg.de>
2016-08-09 16:06:15 +0200

    Fix matrix concatenation.

--- pcd/posread.m
+++ pcd/posread.m
@@ -68,10 +68,10 @@ pos.time = [];
 line = fgetl(fid);
 if ischar(line)
     time = textscan(line, '%s %f64');
-    if ~strcmpi(time{1}, 'Time:')
-        error(formatError);
-    else
+    if strcmpi(time{1}, 'Time:')
         pos.time = time{2};
+    else
+        error(formatError);        
     end
 end
 
@@ -80,12 +80,12 @@ pos.odometry = [];
 line = fgetl(fid);
 if ischar(line)
     odometry = textscan(line, '%s %f %f %f %f %f %f');
-    if ~strcmpi(odometry{1}, 'Odometry:')
-        error(formatError);
-    else
+    if strcmpi(odometry{1}, 'Odometry:')
         odometry = [odometry{2:7}];
         pos.odometry = eye(4);
-        pos.odometry(1:3,:) = [eul2rotm(odometry(4:6)), odometry(1:3)];
+        pos.odometry(1:3,:) = [eul2rotm(odometry(4:6)), odometry(1:3)'];
+    else
+        error(formatError);
     end
 end
 
@@ -94,13 +94,13 @@ pos.gps = [];
 line = fgetl(fid);
 if ischar(line)
     gps = textscan(line, '%s %f64 %f %f %f');
-    if ~strcmpi(gps{1}, 'GPS:')
-        error(formatError);
-    else
+    if strcmpi(gps{1}, 'GPS:')
         pos.gps.time = gps{2};
         pos.gps.longitude = gps{3};
         pos.gps.latitude = gps{4};
         pos.gps.elevation = gps{5};
+    else
+        error(formatError);
     end
 end
 
