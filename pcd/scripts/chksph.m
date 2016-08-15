5abcd27af6d7153f3cecb807168381b1af504dd8
Alexander Schaefer <fudger@users.noreply.github.com>
2016-08-15 21:00:20 +0200

    Add missing coordinate transformation.

--- pcd/scripts/chksph.m
+++ pcd/scripts/chksph.m
@@ -6,10 +6,12 @@
 pcdcart = pcdread('campus_cart.pcd');
 pcdsph = pcdread('campus_sph.pcd');
 
-% Build a point cloud from the Cartesian coordinates.
+% Build a point cloud from the Cartesian coordinates in the world frame.
 pccart = pointCloud([pcdcart.x(:), pcdcart.y(:), pcdcart.z(:)]);
+pccart = pctransform(pccart, ht2affine3d(pcdcart.odometry));
 
-% Convert the spherical coordinates to a Cartesian point cloud.
+% Convert the spherical coordinates to a Cartesian point cloud in the world
+% frame.
 sp = trquat2tform(...
     [pcdsph.sensor_x(:), pcdsph.sensor_y(:), pcdsph.sensor_z(:)], ...
     [pcdsph.sensor_qw(:), pcdsph.sensor_qx(:), pcdsph.sensor_qy(:), ...
@@ -18,5 +20,10 @@ rlim = [2,120];
 ls = laserscan(sp, pcdsph.azimuth, pcdsph.elevation, pcdsph.radius, rlim);
 pcsph = ls2pc(ls);
 
+% Visualize the difference between the point clouds.
+figure;
+pcshowpair(pcsph, pccart);
+
 % Plot the distances between the points of the point clouds in a histogram.
+figure;
 hist(pccompare(pcsph, pccart), 1000);
