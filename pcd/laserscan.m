8d62961f7fd40e6711eb41d682e258e186441179
Alexander Schaefer <aschaef@cs.uni-freiburg.de>
2016-08-16 13:23:34 +0200

    Simplify code.

--- pcd/laserscan.m
+++ pcd/laserscan.m
@@ -219,16 +219,15 @@ classdef laserscan < handle
             % laser scan.
             if parallel.gpu.GPUDevice.isAvailable % Use GPU.
                 p = reshape(pagefun(@mtimes,gpuArray(obj.sp), ps),4,[]).';
-                           
-                % Revert the homogeneous coordinates back to Cartesian.
-                p = gather(p(:,1:3));
             else % Use CPU.
                 p = zeros(obj.count, 4);
                 parfor i = 1 : size(obj.sp, 3)
                     p(i,:) = (obj.sp(:,:,3) * ps(:,1,i)).'; %#ok<PFBNS>
                 end
-                p = p(:,1:3);
             end
+            
+            % Revert the homogeneous coordinates back to Cartesian.
+            p = gather(p(:,1:3));
         end
         
         % Transform laser scan to point cloud.
