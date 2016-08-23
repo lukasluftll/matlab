25cb777774eaeb163b88c39904e93fe8a8d49452
Alexander Schaefer <aschaef@cs.uni-freiburg.de>
2016-08-23 18:07:40 +0200

    Add parameter to decide which reference frame to use.

--- pcd/pcmap.m
+++ pcd/pcmap.m
@@ -1,20 +1,38 @@
-function map = pcmap(folder, step)
+function map = pcmap(folder, res, mode)
 % PCMAP Build map from multiple PCD files.
-%   MAP = PCMAP(FOLDER, STEP) reads all PCD files in directory FOLDER and 
-%   merges them into a single pointCloud object MAP. 
-%   If the PCD files come with DAT files that provide odometry information, 
-%   the MAP point cloud is built w.r.t. the odometry frame.
+%   MAP = PCMAP(FOLDER, RES) reads all PCD files in directory FOLDER and 
+%   merges them into a single pointCloud object MAP.
 %
-%   STEP specifies the grid step parameter used for subsampling. During 
-%   subsampling, the volume occupied by both point clouds is voxelized. 
-%   STEP is the edge length of the voxels. Whenever two points reside in 
-%   one voxel, they are merged to a single point.
+%   RES specifies the grid resolution parameter used for subsampling. 
+%   During subsampling, the volume occupied by both point clouds is 
+%   voxelized. RES is the edge length of the voxels. Whenever two points 
+%   reside in one voxel, they are merged to a single point.
+%
+%   MAP = PCMAP(FOLDER, RES, MODE) defines the mode used when merging the
+%   point cloud. The MODE string can take different values:
+%
+%   'direct' (default) - The point coordinates in the PCD files are 
+%                        specified in the world coordinate system, so they 
+%                        are merged without applying any coordinate 
+%                        transformation.
+%
+%   'odometry'         - The point coordinates in the PCD files are 
+%                        specified in the robot coordinate system, so they 
+%                        are transformed into the world coordinate system 
+%                        by applying the world-to-robot transformation 
+%                        provided by the odometry property of the 
+%                        accompanying DAT files.
+%
+%   Example:
+%      pc = pcmap('pcd/data', 0.1, 'direct')
+%
+%   See also POINTCLOUD, PCREAD, PCDREAD.
 
 % Copyright 2016 Alexander Schaefer
 
 %% Valiate input.
 % Check the number of input arguments.
-narginchk(2, 2)
+narginchk(2, 3)
 
 % Check the FOLDER input argument.
 if ~ischar(folder)
@@ -23,6 +41,25 @@ end
 if ~isdir(folder)
     error('FOLDER does not refer to a folder.')
 end
+
+% Check the RES input argument.
+if numel(res) ~= 1 || ~isnumeric(res) || res <= 0
+    error('RES must be a positive number.')
+end
+
+% Define the MODE input argument, if not specified.
+if nargin < 3
+    mode = 'direct';
+end
+
+% Check the MODE input argument.
+mode = lower(mode);
+switch mode
+    case 'direct'
+    case 'odometry'
+    otherwise
+        error('MODE must be ''direct'' or ''odometry''.')
+end    
 
 %% Merge point clouds.
 % Get the PCD file names.
@@ -41,14 +78,18 @@ for i = 1 : numel(file)
     pcd = pcdread([folder, '/', file(i).name]);
     pc = pointCloud([pcd.x(:), pcd.y(:), pcd.z(:)]);
     
-    % If odometry information is given, transform the point cloud
-    % accordingly.
-    if isfield(pcd, 'odometry')
-        pc = pctransform(pc, ht2affine3d(pcd.odometry));    
+    % Transform the point cloud, if necessary.
+    switch mode
+        case 'direct'
+        case 'odometry'
+            if ~isfield(pcd, 'odometry')
+                error(['No odometry available for file ',file(i).name,'.'])
+            end
+            pc = pctransform(pc, ht2affine3d(pcd.odometry));    
     end
 
     % Merge the point cloud with the map.
-    map = pcmerge(map, pc, step);
+    map = pcmerge(map, pc, res);
     
     % Advance the progress bar.
     waitbar(i/numel(file), waitbarHandle);
