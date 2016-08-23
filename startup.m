accbca0082ddb01905079b80efa3ccbea063301f
Alexander Schaefer <aschaef@cs.uni-freiburg.de>
2016-08-23 18:05:12 +0200

    Automatically add working folders on startup.

---
+++ startup.m
@@ -0,0 +1,10 @@
+% Set the user path to the repository top-level directory.
+userpath(pwd)
+
+% Add the folders of the repository to the MATLAB search path.
+addpath('util')
+addpath('pcd/script')
+addpath('pcd/test')
+addpath('pcd')
+addpath('pf')
+addpath('plot')
