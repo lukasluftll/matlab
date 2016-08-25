250d97c557c90128511243d698a23b7d1e0f004d
Alexander Schaefer <aschaef@cs.uni-freiburg.de>
2016-08-25 15:35:11 +0200

    Integrate option to build likelihood field.

--- pcd/script/buildlidarmap.m
+++ pcd/script/buildlidarmap.m
@@ -7,7 +7,7 @@ resultFolder = 'pcd/result';
 % Name of the file that contains the merged point cloud.
 pcFile = [resultFolder, '/pcmap_campus.mat'];
 
-% Sensor model to use to build map: 'decay' | 'ref'.
+% Sensor model to use to build map: 'decay' | 'ref' | 'lf'.
 model = 'ref';
 
 % Resolution of the resulting lidar map.
@@ -16,33 +16,36 @@ lidarMapRes = 0.5;
 % Sensor reading range.
 rlim = [2, 120];
 
-%% Select mapping algorithm.
-switch model
-    case 'decay'
-        mapFun = @decaymap;
-    case 'ref'
-        mapFun = @refmap;
-    otherwise
-        error(['Map model ', model, ' not supported.'])
-end
+% Variance for likelihood field computation.
+sigma = 1;
 
 %% Prepare output file.
 % Load the file that contains the merged point cloud.
 load(pcFile);
 
+% Print caption.
+disp(['Computing ', model, ' map for ', dataset, ' ...'])
+
 % Define the name of the output MAT file.
 lidarMapFile = [resultFolder, '/', model, 'map_', dataset, '.mat'];
 
-% Save parameters to file.
-save(lidarMapFile, 'dataset', 'folder', 'pcMapRes', 'model', ...
-    'lidarMapRes', 'rlim', '-v7.3');
-
 %% Compute map extent.
-% Compute the extent of the denoised point cloud.
 lim = [pcMap.XLimits; pcMap.YLimits; pcMap.ZLimits];
-lim = lim + repmat([-1,+1],3,1)*lidarMapRes;
+lim = lim + repmat([-1,+1], 3, 1) * lidarMapRes;
 
 %% Create lidar map.
+% Define the mapping function to use.
+switch lower(model)
+    case 'decay'
+        mapFun = @decaymap;
+    case 'ref'
+        mapFun = @refmap;
+    case 'lf'
+        mapFun = @lfmap;
+    otherwise
+        error('Mapping function not supported.')
+end
+    
 % Get the PCD file names.
 pcdFile = dir([folder, '/*.pcd']);
 
@@ -50,32 +53,42 @@ pcdFile = dir([folder, '/*.pcd']);
 xgv = lim(1,1) : lidarMapRes : lim(1,2);
 ygv = lim(2,1) : lidarMapRes : lim(2,2);
 zgv = lim(3,1) : lidarMapRes : lim(3,2);
-  
-% Iterate over all laser scans, compute local maps and merge them into a 
-% global map.
-gridsize = [numel(xgv), numel(ygv), numel(zgv)] - 1;
-num = zeros(gridsize);
-denom = zeros(gridsize);
-nPcdFile = numel(pcdFile);
-parprogress(nPcdFile);
-parfor i = 1 : nPcdFile
-    % Read laser scan data from file.
-    ls = lsread([folder, '/', pcdFile(i).name], rlim);
 
-    % Build the local lidar map.
-    warning('off') %#ok<WNOFF>
-    [~,numi,denomi] = mapFun(ls, xgv, ygv, zgv);
-    warning('on') %#ok<WNON>
+% For the decay and the reflectivity model, loop over all scans and compute
+% the numerator and denominator of each map cell's value.
+if strcmpi(model, 'decay') || strcmpi(model, 'ref')    
+    % Iterate over all laser scans, compute local maps and merge them into 
+    % a global map.
+    gridsize = [numel(xgv), numel(ygv), numel(zgv)] - 1;
+    num = zeros(gridsize);
+    denom = zeros(gridsize);
+    nPcdFile = numel(pcdFile);
+    parprogress(nPcdFile);
+    parfor i = 1 : nPcdFile
+        % Read laser scan data from file.
+        ls = lsread([folder, '/', pcdFile(i).name], rlim);
+
+        % Build the local lidar map.
+        warning('off') %#ok<WNOFF>
+        [~,numi,denomi] = mapFun(ls, xgv, ygv, zgv);
+        warning('on') %#ok<WNON>
+
+        % Integrate the local map information into the global map.
+        num = num + numi;
+        denom = denom + denomi;
+
+        % Update progress bar.
+        parprogress;
+    end
+    parprogress(0);
     
-    % Integrate the local map information into the global map.
-    num = num + numi;
-    denom = denom + denomi;
-    
-    % Update progress bar.
-    parprogress;
+    % Compute the lidar map.
+    lidarMap = voxelmap(num./denom, xgv, ygv, zgv);
+else
+    % Compute the likelihood field.
+    lidarMap = mapFun(pcMap, sigma, xgv, ygv, zgv);
 end
-parprogress(0);
 
-%% Save the resulting lidar map.
-lidarMap = voxelmap(num./denom, xgv, ygv, zgv);
-save(lidarMapFile, 'lidarMap', '-append');
+%% Save map.
+save(lidarMapFile, 'dataset', 'folder', 'pcMapRes', 'model', ...
+    'lidarMapRes', 'rlim', 'sigma', 'lidarMap', '-v7.3');
