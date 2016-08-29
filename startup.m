a4b844946fab0a5bda6761109b04836bb8c570b9
Alexander Schaefer <aschaef@cs.uni-freiburg.de>
2016-08-29 15:02:51 +0200

    Transform relative paths to absolute paths.

--- startup.m
+++ startup.m
@@ -1,8 +1,7 @@
 % Add the folders of the repository to the MATLAB search path.
-addpath('util')
-addpath('pcd/script')
-addpath('pcd/test')
-addpath('pcd')
-addpath('pf')
-addpath('plot')
-
+addpath([pwd, '/util'])
+addpath([pwd, '/pcd/script'])
+addpath([pwd, '/pcd/test'])
+addpath([pwd, '/pcd'])
+addpath([pwd, '/pf'])
+addpath([pwd, '/plot'])
