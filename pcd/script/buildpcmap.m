fb10387e358e69de8066d078ffe3aa6913e5cbee
Alexander Schaefer <fudger@users.noreply.github.com>
2016-08-29 09:18:16 +0200

    Simplify variable names.

--- pcd/script/buildpcmap.m
+++ pcd/script/buildpcmap.m
@@ -8,7 +8,7 @@ dataset = 'demo';
 folder = ['pcd/data/', dataset, '/pcd_sph'];
 
 % Resolution of the merged point cloud map.
-pcMapRes = 0.5;
+pcRes = 0.5;
 
 %% Prepare output file.
 % Print caption.
@@ -30,8 +30,8 @@ pcMapFile = [resultFolder, '/pcmap_', dataset, '.mat'];
 pcdFile = dir([folder, '/*.pcd']);
     
 % Build the map and denoise it.
-pcMap = pcmap(folder, pcMapRes);
+pcMap = pcmap(folder, pcRes);
 
 % Save the point cloud map to file.
-save(pcMapFile, 'dataset', 'folder', 'pcMapRes', 'pcMap', '-v7.3')
+save(pcMapFile, 'dataset', 'folder', 'pcRes', 'pcMap', '-v7.3')
 display(['Result written to ', pcMapFile, '.'])
