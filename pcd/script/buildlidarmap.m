1eddf2dd5b6e1cc9a8da0687ed1d9551cb77f83b
Alexander Schaefer <fudger@users.noreply.github.com>
2016-08-29 22:14:26 +0200

    Add computation of NaN prior for endpoint model.

--- pcd/script/buildlidarmap.m
+++ pcd/script/buildlidarmap.m
@@ -8,7 +8,7 @@ resultFolder = 'pcd/result';
 pcFile = [resultFolder, '/pcmap_demo.mat'];
 
 % Sensor model to use to build map: 'decay' | 'ref' | 'lf'.
-model = 'decay';
+model = 'lf';
 
 % Resolution of the resulting lidar map.
 mapRes = 2.0;
@@ -62,55 +62,88 @@ xgv = mapLim(1,1) : mapRes : mapLim(1,2)+mapRes;
 ygv = mapLim(2,1) : mapRes : mapLim(2,2)+mapRes;
 zgv = mapLim(3,1) : mapRes : mapLim(3,2)+mapRes;
 
-% Define the mapping function to use.
-switch lower(model)
-    case 'decay'
-        mapFun = @decaymap;
-    case 'ref'
-        mapFun = @refmap;
-    case 'lf'
-        mapFun = @lfmap;
-    otherwise
-        error('Mapping function not supported.')
-end
-
 % For the decay and the reflectivity model, loop over all scans and compute
 % the numerator and denominator of each map cell's value.
 % For the endpoint model, simply compute the Gaussian of the distance to
 % the nearest obstacle for each voxel.
-if strcmpi(model, 'decay') || strcmpi(model, 'ref')    
-    % Iterate over all laser scans, compute local maps and merge them into 
-    % a global map.
-    gridsize = [numel(xgv), numel(ygv), numel(zgv)] - 1;
-    num = zeros(gridsize);
-    denom = zeros(gridsize);
-    parprogress(nPcdFile);
-    parfor i = 1 : nPcdFile
-        % Read laser scan data from file.
-        ls = lsread([folder, '/', pcdFile(i).name], rlim);
+gridsize = [numel(xgv), numel(ygv), numel(zgv)] - 1;
+parprogress(nPcdFile);
+switch lower(model)
+    case 'decay'
+        % Iterate over all laser scans, compute local decay rate maps and 
+        % merge them into a global map.
+        r = zeros(gridsize);
+        l = zeros(gridsize);
+        parfor i = 1 : nPcdFile
+            % Read laser scan data from file.
+            ls = lsread([folder, '/', pcdFile(i).name], rlim);
 
-        % Build the local lidar map.
-        warning('off') %#ok<WNOFF>
-        [~,numi,denomi] = mapFun(ls, xgv, ygv, zgv);
-        warning('on') %#ok<WNON>
+            % Build the local decay rate map.
+            warning('off') %#ok<*WNOFF>
+            [~,ri,li] = decaymap(ls, xgv, ygv, zgv);
+            warning('on') %#ok<*WNON>
+            
+            % Integrate the local map information into the global map.
+            r = r + ri;
+            l = l + li;
 
-        % Integrate the local map information into the global map.
-        num = num + numi;
-        denom = denom + denomi;
-
-        % Update progress bar.
-        parprogress;
-    end
-    parprogress(0);
-    
-    % Compute the lidar map.
-    lidarMap = voxelmap(num./denom, xgv, ygv, zgv);
-else
-    % Compute the likelihood field.
-    lidarMap = mapFun(pcMap, sigma, xgv, ygv, zgv);
+            % Update progress bar.
+            parprogress;
+        end
+        
+        % Compute the decay rate map.
+        lidarMap = voxelmap(r./l, xgv, ygv, zgv);
+    case 'ref'
+        % Iterate over all laser scans, compute local reflectivity maps and
+        % merge them into a global map.
+        h = zeros(gridsize);
+        m = zeros(gridsize);
+        parfor i = 1 : nPcdFile
+            % Read laser scan data from file.
+            ls = lsread([folder, '/', pcdFile(i).name], rlim);
+            
+            % Build the local reflectivity map.
+            warning('off')
+            [~,hi,mi] = refmap(ls, xgv, ygv, zgv);
+            warning('on')
+            
+            % Integrate the local map information into the global map.
+            h = h + hi;
+            m = m + mi;
+            
+            % Update the progress bar.
+            parprogress;
+        end
+        
+        % Compute the reflectivity map.
+        lidarMap = voxelmap(h ./ (h+m), xgv, ygv, zgv);
+    case 'lf'
+        % Compute the likelihood field.
+        lidarMap = lfmap(pcMap, sigma, xgv, ygv, zgv);
+        
+        % Count the number of no-returns and the number of returned rays.
+        nRet = 0;
+        nNret = 0;
+        parfor i = 1 : nPcdFile
+            % Read laser scan data from file.
+            ls = lsread([folder, '/', pcdFile(i).name], rlim);
+            
+            % Sum up the number of returns and no-returns.
+            nRet = nRet + sum(ls.ret);
+            nNret = nNret + sum(~ls.ret);
+            
+            % Update the progress bar.
+            parprogress;
+        end
+        
+        % Compute the unconditioned probability of no-returns.
+        pNan = nNret / (nRet+nNret);
+    otherwise
+        error(['Sensor model ', model, ' not supported.'])
 end
+parprogress(0);
 
 %% Save map.
 save(lidarMapFile, 'dataset', 'folder', 'pcRes', 'model', ...
-    'mapRes', 'rlim', 'sigma', 'lidarMap', '-v7.3');
+    'mapRes', 'rlim', 'sigma', 'lidarMap', 'pNan', '-v7.3');
 display(['Result written to ', lidarMapFile, '.'])
