8e2ea4186e1fae008e8f0dd3172628243935c3e4
Alexander Schaefer <aschaef@cs.uni-freiburg.de>
2016-09-04 16:49:18 +0200

    Save script results despite error.

--- pcd/script/klilidarmap.m
+++ pcd/script/klilidarmap.m
@@ -13,66 +13,69 @@ display(['Computing inverse KL divergence of ', model, 'map of ', ...
     dataset, ' dataset ...'])
 
 %% Compute inverse KL divergence.
-% Compute the inverse KL divergence for each scan.
-Dhg = NaN(numel(1:nShift:numel(evalFile)), 1);
-parprogress(numel(evalFile));
-for i = 1 : nShift : numel(evalFile)
-    % Read laser scan data from file.
-    ls = lsread([dataFolder, '/', evalFile(i).name], rlim);
-    
-    % Randomly shift the scan in the x-y plane and compute the overall
-    % likelihood.
-    j = 0;
-    offset = NaN(nShift, 2);
-    ps = NaN(nShift, 1);
-    while j < nShift
-        % Generate random x-y offset.
-        offsetTmp = rkli * ([2*rand,2*rand]-1);
+try
+    % Compute the inverse KL divergence for each scan.
+    Dhg = NaN(numel(1:nShift:numel(evalFile)), 1);
+    parprogress(numel(evalFile));
+    for i = 1 : nShift : numel(evalFile)
+        % Read laser scan data from file.
+        ls = lsread([dataFolder, '/', evalFile(i).name], rlim);
         
-        % Make sure offset position is within radius.
-        if norm(offsetTmp) > rkli
-            continue
+        % Randomly shift the scan in the x-y plane and compute the overall
+        % likelihood.
+        j = 0;
+        offset = NaN(nShift, 2);
+        ps = NaN(nShift, 1);
+        while j < nShift
+            % Generate random x-y offset.
+            offsetTmp = rkli * ([2*rand,2*rand]-1);
+            
+            % Make sure offset position is within radius.
+            if norm(offsetTmp) > rkli
+                continue
+            end
+            
+            % Increment number of shifts.
+            j = j + 1;
+            
+            % Shift laser scan.
+            offset(j,:) = offsetTmp;
+            lss = ls;
+            lss.sp = pagetimes(lss.sp, ...
+                repmat(trvec2tform([offset(j,:),0]),1,1,size(lss.sp,3)));
+            
+            % Compute the measurement likelihood.
+            switch lower(model)
+                case 'decay'
+                    [pr, Lr] = decayray(lss, constrain(lidarMap,decayLim));
+                case 'ref'
+                    [pr, Lr] = refray(lss, constrain(lidarMap,refLim));
+                case 'lf'
+                    [pr, Lr] = lfray(lss, constrain(lidarMap,lfLim), pnr);
+                otherwise
+                    error(['Sensor model ', model, ' not supported.'])
+            end
+            
+            % Integrate the probability of the scan over the circle area.
+            ps(j) = sum([pr; exp(Lr)]);
         end
         
-        % Increment number of shifts.
-        j = j + 1;
+        % Normalize the probabilities.
+        ps = ps / sum(ps);
         
-        % Shift laser scan.
-        offset(j,:) = offsetTmp;
-        lss = ls;
-        lss.sp = pagetimes(lss.sp, ...
-            repmat(trvec2tform([offset(j,:),0]), 1, 1, size(lss.sp,3)));
+        % Normalize the Gaussian.
+        Ns = mvnpdf(offset, 0, eye(2)*sigmaLoc);
+        Ns = Ns / sum(Ns);
         
-        % Compute the measurement likelihood.
-        switch lower(model)
-            case 'decay'
-                [pr, Lr] = decayray(lss, constrain(lidarMap, decayLim));
-            case 'ref'
-                [pr, Lr] = refray(lss, constrain(lidarMap, refLim));
-            case 'lf'
-                [pr, Lr] = lfray(lss, constrain(lidarMap, lfLim), pnr);
-            otherwise
-                error(['Sensor model ', model, ' not supported.'])
-        end
+        % Compute the inverse KL divergence of the scan.
+        Dhg(i) = sum(ps .* (log(ps) - log(Ns)));
         
-        % Integrate the probability of the scan over the circle area.
-        ps(j) = sum([pr; exp(Lr)]);
+        % Update the progress bar.
+        parprogress;
     end
-    
-    % Normalize the probabilities.
-    ps = ps / sum(ps);
-    
-    % Normalize the Gaussian.
-    Ns = mvnpdf(offset, 0, eye(2)*sigmaLoc);
-    Ns = Ns / sum(Ns);
-
-    % Compute the inverse KL divergence of the scan.
-    Dhg(i) = sum(ps .* (log(ps) - log(Ns)));
-    
-    % Update the progress bar.
-    parprogress;
+    parprogress(0);
+catch
 end
-parprogress(0);
 
 % Save the KL divergence to file.
 save(kliFile, 'lidarMapFile', 'dataset', 'model', 'pcdPerLs', ...
--- pcd/script/kllidarmap.m
+++ pcd/script/kllidarmap.m
@@ -13,34 +13,37 @@ display(['Computing KL divergence of ', model, 'map of ', dataset, ...
     ' dataset ...'])
 
 %% Compute KL divergence.
-% Compute the KL divergence for each scan.
-Dgh = NaN(numel(evalFile), 1);
-parprogress(numel(evalFile));
-pnr = 0;
-parfor i = 1 : numel(evalFile)
-    % Read laser scan.
-    ls = lsread([dataFolder, '/', evalFile(i).name], rlim);
-   
-    % Compute the measurement likelihood.
-
-    switch lower(model)
-        case 'decay'
-            [pi, Li] = decayray(ls, constrain(lidarMap, decayLim));
-        case 'ref'
-            [pi, Li] = refray(ls, constrain(lidarMap, refLim));
-        case 'lf'
-            [pi, Li] = lfray(ls, constrain(lidarMap, lfLim), pnr);
-        otherwise
-            error(['Sensor model ', model, ' not supported.'])
+try
+    % Compute the KL divergence for each scan.
+    Dgh = NaN(numel(evalFile), 1);
+    parprogress(numel(evalFile));
+    pnr = 0;
+    parfor i = 1 : numel(evalFile)
+        % Read laser scan.
+        ls = lsread([dataFolder, '/', evalFile(i).name], rlim);
+        
+        % Compute the measurement likelihood.
+        
+        switch lower(model)
+            case 'decay'
+                [pi, Li] = decayray(ls, constrain(lidarMap, decayLim));
+            case 'ref'
+                [pi, Li] = refray(ls, constrain(lidarMap, refLim));
+            case 'lf'
+                [pi, Li] = lfray(ls, constrain(lidarMap, lfLim), pnr);
+            otherwise
+                error(['Sensor model ', model, ' not supported.'])
+        end
+        
+        % Compute the KL divergence of the whole scan.
+        Dgh(i) = -sum([Li; log(pi)]);
+        
+        % Update the progress bar.
+        parprogress;
     end
-    
-    % Compute the KL divergence of the whole scan.
-    Dgh(i) = -sum([Li; log(pi)]);
-    
-    % Update the progress bar.
-    parprogress;
+    parprogress(0);
+catch
 end
-parprogress(0);
 
 % Save the KL divergence to file.
 save(klFile, 'lidarMapFile', 'dataset', 'model', 'decayLim', ...
