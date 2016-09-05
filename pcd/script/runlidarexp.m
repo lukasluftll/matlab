8692e7a57e5f509f26ce27d8d3ebd105e2288197
Alexander Schaefer <aschaef@cs.uni-freiburg.de>
2016-09-05 17:44:03 +0200

    Add problidarmap to run-all script.

--- pcd/script/runlidarexp.m
+++ pcd/script/runlidarexp.m
@@ -4,26 +4,26 @@
 dataset = 'campus'; %#ok<*NASGU>
 buildpcmap
 model = 'decay';
-buildlidarmap, kllidarmap, klilidarmap
+buildlidarmap, kllidarmap, klilidarmap, problidarmap
 model = 'ref';
-buildlidarmap, kllidarmap, klilidarmap
+buildlidarmap, kllidarmap, klilidarmap, problidarmap
 model = 'lf';
-buildlidarmap, kllidarmap, klilidarmap
+buildlidarmap, kllidarmap, klilidarmap, problidarmap
 
 dataset = 'schauinsland';
 buildpcmap
 model = 'decay';
-buildlidarmap, kllidarmap, klilidarmap
+buildlidarmap, kllidarmap, klilidarmap, problidarmap
 model = 'ref';
-buildlidarmap, kllidarmap, klilidarmap
+buildlidarmap, kllidarmap, klilidarmap, problidarmap
 model = 'lf';
-buildlidarmap, kllidarmap, klilidarmap
+buildlidarmap, kllidarmap, klilidarmap, problidarmap
 
 dataset = 'mooswald';
 buildpcmap
 model = 'decay';
-buildlidarmap, kllidarmap, klilidarmap
+buildlidarmap, kllidarmap, klilidarmap, problidarmap
 model = 'ref';
-buildlidarmap, kllidarmap, klilidarmap
+buildlidarmap, kllidarmap, klilidarmap, problidarmap
 model = 'lf';
-buildlidarmap, kllidarmap, klilidarmap
+buildlidarmap, kllidarmap, klilidarmap, problidarmap
