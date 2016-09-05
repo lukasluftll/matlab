aa65bbd2edcb39200a273ecd85686c065c88c010
Alexander Schaefer <aschaef@cs.uni-freiburg.de>
2016-09-05 15:29:07 +0200

    Merge KL divergence of several scans.

--- pcd/script/kllidarmap.m
+++ pcd/script/kllidarmap.m
@@ -3,26 +3,29 @@
 %% Fetch parameters.
 lidarparams
 
-%% Prepare output file.
-% Load the file that contains the lidar map.
-load(lidarMapFile, 'lidarMap', 'pnr');
-
+%% Compute KL divergence.
 % Print caption.
 hline(75, '#')
 display(['Computing KL divergence of ', model, 'map of ', dataset, ...
     ' dataset ...'])
 
-%% Compute KL divergence.
+% Load the lidar map and the unconditioned probability of no-return rays.
+load(lidarMapFile, 'lidarMap', 'pnr');
+
+% Determine the number of files used for evaluation.
+n = numel(evalFile);
+
+% Catch any error that occurs while computing the KL divergence.
+ex = [];
 try
     % Compute the KL divergence for each scan.
-    Dgh = NaN(numel(evalFile), 1);
-    parprogress(numel(evalFile));
-    parfor i = 1 : numel(evalFile)
+    parprogress(n);
+    DghScan = NaN(n, 1);
+    parfor i = 1 : n
         % Read laser scan.
         ls = lsread([dataFolder, '/', evalFile(i).name], rlim);
         
         % Compute the measurement likelihood.
-        
         switch lower(model)
             case 'decay'
                 [pi, Li] = decayray(ls, constrain(lidarMap, decayLim));
@@ -34,17 +37,22 @@ try
                 error(['Sensor model ', model, ' not supported.'])
         end
         
-        % Compute the KL divergence of the whole scan.
-        Dgh(i) = -sum([Li; log(pi)]);
+        % Compute the KL divergence of the scan.
+        DghScan(i) = -sum([Li; log(pi)]);
         
         % Update the progress bar.
         parprogress;
     end
     parprogress(0);
-catch
+catch ex
+    display(ex.msgtext)
 end
+
+% Merge the KL divergences of several scans to get the divergence of at
+% least one full scanner revolution.
+Dgh = sum(reshape(DghScan(1:n>floor(n/pcdPerLs)*pcdPerLs), pcdPerLs, []));
 
 % Save the KL divergence to file.
 save(klFile, 'lidarMapFile', 'dataset', 'model', 'decayLim', ...
-    'refLim', 'lfLim', 'Dgh', '-v7.3');
+    'refLim', 'lfLim', 'pcdPerLs', 'DghScan', 'Dgh', 'ex', '-v7.3');
 display(['Result written to ', klFile, '.'])
