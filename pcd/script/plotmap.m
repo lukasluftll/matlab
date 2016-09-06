37314c36977d52b5512ea602381290d84b935375
Alexander Schaefer <aschaef@cs.uni-freiburg.de>
2016-09-06 20:52:11 +0200

    Add script to plot point cloud map and robot trajectory.

---
+++ pcd/script/plotmap.m
@@ -0,0 +1,22 @@
+% Plot map with robot trajectory.
+
+%% Fetch parameters.
+lidarparams
+
+%% Read robot trajectory.
+rt = [];
+parprogress(numel(mappingFile));
+for i = 1 : numel(mappingFile)
+    ls = lsread([dataFolder, '/', mappingFile(i).name], rlim); %#ok<*PFGV>
+    rt = unique([rt;unique(tform2trvec(ls.sp),'rows')],'rows'); %#ok<AGROW>
+    parprogress;
+end
+parprogress(0);
+
+%% Plot map.
+colormap('hot')
+pcshow(pcMap)
+hold on
+plot3(rt(:,1), rt(:,2), rt(:,3), '.')
+labelaxes('m')
+hold off
