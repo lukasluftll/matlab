5975737dff8e53d5ca093737f416d92b9ca16e4e
Alexander Schaefer <aschaef@cs.uni-freiburg.de>
2016-09-15 08:12:37 +0200

    Simplify code.

--- util/test/testprogressbar.m
+++ util/test/testprogressbar.m
@@ -3,10 +3,11 @@
 % Copyright 2016 Alexander Schaefer
 
 % SHARED VARIABLES SECTION.
+n = 1000;
+nWarn = 300;
 
 %% Single-threaded execution
 disp('Single-threaded execution')
-n = 1000;
 progressbar(n)
 for i = 1 : n
     progressbar
@@ -14,19 +15,17 @@ end
 
 %% Single-threaded execution with warnings
 disp('Single-threaded execution with warnings')
-n = 1000;
 progressbar(n)
 for i = 1 : n
     progressbar
     
-    if rem(i,300) == 0
+    if rem(i,nWarn) == 0
         warning('warning text')
     end
 end
 
 %% Execution in parfor
 disp('Multi-threaded execution')
-n = 1000;
 progressbar(n)
 parfor i = 1 : n
     progressbar
@@ -34,12 +33,11 @@ end
 
 %% Execution in parfor with warnings
 disp('Multi-threaded execution with warnings')
-n = 1000;
 progressbar(n)
 parfor i = 1 : n
     progressbar
     
-    if rem(i,300) == 0
+    if rem(i,nWarn) == 0
         warning('warning text')
     end
 end
