2440ed97e8761274c174d2fb9286a7a3fb51765d
Alexander Schaefer <aschaef@cs.uni-freiburg.de>
2016-09-20 16:04:03 +0200

    Simplify and clarify function.

--- util/pagetimes.m
+++ util/pagetimes.m
@@ -1,28 +1,15 @@
 function c = pagetimes(a, b)
-% PAGETIMES Page-wise matrix-matrix multiply.
-%   C = PAGETIMES(A, B) multiplies K matrices by K matrices or K matrices
-%   by K vectors.
+% PAGETIMES Page-wise matrix multiply.
+%   C = PAGETIMES(A,B) multiplies the pages of two matrices A and B.
 %
-%   A is a IxJxK page-wise concatenation of IxJ matrices.
-%
-%   Matrix-by-matrix multiplication
-%   -------------------------------
-%   B is a IxJxK page-wise concatenation IxJ matrices.
-%
-%   C is a IxJxK page-wise concatenation of matrices, where the k-th page 
-%   contains the product of A(:,:,k) * B(:,:,k).
-%
-%   Matrix-by-vector multiplication
-%   -------------------------------
-%   B is a JxK matrix. The columns of B contain J-element vectors.
-%
-%   C is a IxK matrix. The k-th column of C contains the product of 
-%   A(:,:,k) * B(:,k).
+%   A is a IxJxN matrix. 
+%   B is a JxKxN matrix. 
+%   C is a IxKxN matrix whose n-th page contains A(:,:,n)*B(:,:,n).
 %
 %   Example:
-%      pagetimes([eye(3), rand(3,1); 0,0,0,1], rand(4,1))
+%      pagetimes(magic(3,3,3), rand(3,3,3))
 %
-%   See also TIMES, MTIMES, ISHRT.
+%   See also TIMES, MTIMES.
 
 % Copyright 2016 Alexander Schaefer
 
@@ -30,47 +17,15 @@ function c = pagetimes(a, b)
 % Check number of input arguments.
 narginchk(2, 2)
 
-% Check contents of A.
-if ~isnumeric(a)
-    error('A must contain numeric values only.')
+% Check the size of the input matrices.
+if size(a,3)~=size(b,3) || size(a,2)~=size(b,1)
+    error('Sizes of A and B do not match.')
 end
 
-% Check dimensionality of A.
-if ndims(a) > 3
-    error('A must be a 3D matrix.')
-end
-
-% Check contents of B.
-if ~isnumeric(b)
-    error('B must contain numeric values only.')
-end
-
-% Check dimensionality of B.
-if ndims(b) > 3 
-    error('B must be a 2D or a 3D matrix.')
-end
-
-% Make sure the sizes of A and B match.
-msg = 'Sizes of A and B do not match.';
-if ismatrix(b) 
-    if size(a,2)~=size(b,1) || size(a,3) ~= size(b,2)
-        error(msg)
-    end
-elseif ~all(size(a)==size(b))
-    error(msg)
-end
-
-%% Perform multiplication.
-if ismatrix(b)   % Matrix-vector multiply.
-    c = zeros(size(a,1), size(b,2));
-    for i = 1 : size(b, 2)
-        c(:,i) = a(:,:,i) * b(:,i);
-    end
-else   % Matrix-matrix multiply.
-    c = zeros(size(a));
-    for i = 1 : size(a, 3)
-        c(:,:,i) = a(:,:,i) * b(:,:,i);
-    end
+%% Perform page-wise multiplication.
+c = zeros(size(a,1), size(b,2), size(a,3));
+parfor i = 1 : size(a, 3)
+    c(:,:,i) = a(:,:,i) * b(:,:,i);
 end
 
 end
