c340650135dadf467f45f7eb344fbaee3e1e4b33
Alexander Schaefer <aschaef@cs.uni-freiburg.de>
2016-09-23 20:54:30 +0200

    Finish implementation of size check for multiple matrices.

--- util/sizechk.m
+++ util/sizechk.m
@@ -1,8 +1,29 @@
-function sizechk(a, b)
+function sizechk(varargin)
 
-if ndims(a)~=ndims(b) || any(size(a)~=size(b))
-    error(['Sizes of ', inputname(a), ' and ', inputname(b), ...
-        ' do not match.'])
+nargoutchk(0, 1)
+narginchk(0, +Inf)
+
+if nargin > 0
+    ref = varargin{1};
+    refname = inputname(1);
+end
+
+for i = 2 : nargin
+    comp = varargin{i};
+    compname = inputname(i);
+    
+    inputnames = blanks(0);
+    if isempty(refname) || isempty(compname)
+        inputnames = ['of ',refname,' and ',compname,' '];
+    end
+       
+    if ndims(ref) ~= ndims(comp)
+        error(['Numbers of dimensions ', inputnames, 'do not match.'])
+    end
+    
+    if any(size(varargin{1}) ~= size(varargin{i}))
+        error(['Sizes ', inputnames, 'do not match.'])
+    end
 end
 
 end
