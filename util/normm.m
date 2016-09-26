af9a4cdf7d508d7cd330ed1fffc60bab7de3e545
Alexander Schaefer <aschaef@cs.uni-freiburg.de>
2016-09-26 11:15:24 +0200

    Add error checks and documentation.

--- util/normm.m
+++ util/normm.m
@@ -1,5 +1,24 @@
 function y = normm(x)
+% NORMM Normalize matrix.
+%    Y = NORMM(X) normalizes the matrix X, so that all elements of Y stay 
+%    within [0; 1.0].
+%
+%    Example:
+%       normm(magic(5))
+%
+%   See also NORMR, NORMC.
 
+% Copyright 2016 Alexander Schaefer
+
+%% Validate input and output.
+nargoutchk(0, 1)
+narginchk(1, 1)
+
+if iscell(x)
+    error('X must not be of type CELL.')
+end
+
+%% Normalize input.
 y = (x - min(x(:))) / range(x);
 
 end
