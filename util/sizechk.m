28361f34a1ecb4d70aead858b6ea90c838493ba7
Alexander Schaefer <aschaef@cs.uni-freiburg.de>
2016-09-26 11:26:36 +0200

    Add documentation.

--- util/sizechk.m
+++ util/sizechk.m
@@ -12,27 +12,35 @@ function sizechk(varargin)
 
 % Copyright 2016 Alexander Schaefer
 
+%% Validate input and output.
 nargoutchk(0, 0)
 narginchk(0, +Inf)
 
+%% Check sizes pairwise.
+% Get the name of the input argument.
 if nargin > 0
     ref = varargin{1};
     refname = upper(inputname(1));
 end
 
+% Compare the size of the first matrix to all other matrices.
 for i = 2 : nargin
+    % Get the name of the other matrix.
     comp = varargin{i};
     compname = upper(inputname(i));
     
+    % Generate a string that identifies the mismatching matrices.
     inputnames = blanks(0);
     if ~isempty(refname) || ~isempty(compname)
-        inputnames = ['of ',refname,' and ',compname,' '];
+        inputnames = ['of ', refname, ' and ', compname, ' '];
     end
-       
+    
+    % Check the number of dimensions of both matrices.
     if ndims(ref) ~= ndims(comp)
         error(['Numbers of dimensions ', inputnames, 'do not match.'])
     end
     
+    % Check the size of both matrices.
     if any(size(varargin{1}) ~= size(varargin{i}))
         error(['Matrix sizes ', inputnames, 'do not match.'])
     end
