dfb2f6f0eddfa3537ee6650e234d69dc9521bca5
Alexander Schaefer <aschaef@cs.uni-freiburg.de>
2016-10-04 11:10:56 +0200

    Revise and tidy up.

--- pcd/script/elevationmodel.m
+++ pcd/script/elevationmodel.m
@@ -1,123 +1,95 @@
 % Evaluate elevation map sensor model for leek field dataset.
 
-% Define parameters.
+%% Define parameters.
+% Step size when shifting the scan.
 shiftres = 0.1;
+
+% Minimum and maximum shifting offset in x and y direction.
 shiftlim = [-10, 100; -10, 20];
+
+% Step size when rotating the scan.
+rotres = 0.01;
+
+% Minimum and maximum rotation.
+rotlim = [-pi, pi];
+
+% PCD file containing the map.
+mapfile = 'pcd/data/leek.pcd';
+
+% PCD file containing the scan.
 sensorfile = 'pcd/data/sensmiddle.pcd';
-rpysens = [deg2rad(10),0,0];
 
-% Create elevation map of field.
+% Orientation of the scan.
+rpysens = deg2rad([0,0,0]);
+
+%% Create elevation map of field.
+% Read PCD file to point cloud.
 pcfield = pcd2pc(pcdread('pcd/data/leek.pcd'));
-pcfield = pctransform(pcfield, ht2affine3d(eul2tform([pi,0,0]))); 
-emnan = elevationmap(pcfield, 0.05);
-em = emnan.fillnan([5,5]);
 
-% Read sensor measurements.
-pcsens = pcd2pc(pcdread(sensorfile)); 
+% Apply rotation to map.
+pcfield = pctransform(pcfield, ht2affine3d(eul2tform([pi,0,0])));
+
+% Create the elevation map and fill the gaps in the map.
+%em = fillnan(elevationmap(pcfield, 0.05), [5,5]);
+
+%% Read sensor measurements.
+% Read PCD file to point cloud.
+pcsens = pcd2pc(pcdread(sensorfile));
+
+% Rotate the scan.
 pcsens = pctransform(pcsens, ht2affine3d(eul2tform(rpysens)));
-psens = reshape(pcsens.Location(:), pcsens.Count, 3, 1);
 
-% Shift the measurements and compute the height difference at each point.
+%% Shift scan.
+% Define the offset vectors.
 x = shiftlim(1,1) : shiftres : shiftlim(1,2);
 y = shiftlim(2,1) : shiftres : shiftlim(2,2);
 nx = numel(x);
 ny = numel(y);
+
+% Initialize the matrix that stores the mean z difference.
 d = NaN(nx,ny);
+
+% Initialize the matrix that stores the fraction of NaN differences.
 nanfrac = NaN(nx,ny);
-n = size(psens, 1);
+
+% Vary the position of the scan and compute the z difference for
+% each offset.
 progressbar(nx)
 parfor ix = 1 : nx
     for iy = 1 : ny
-        % Compute the disparity between elevation map and scan.
-        zoffset = -mean(em.diff(psens+repmat([x(ix),y(iy),0],n,1)), ...
-            'omitnan'); %#ok<PFBNS>
-        p = psens + repmat([x(ix),y(iy),zoffset], n, 1);
-        dz = constrain(em.diff(p), [0,+Inf]);
-        if sum(isnan(dz)) < 0.1*n
+        % Extract the Mx3 vector of point coordinates from the scan.
+        psens = reshape(pcsens.Location(:),pcsens.Count,3,1); %#ok<*PFBNS>
+        
+        % Shift the scan horizontally.
+        psens = psens + repmat([x(ix),y(iy),0], size(psens,1), 1);
+        
+        % Adjust the z coordinate of the scan origin.
+        ifin = all(isfinite(psens(1:2,:)), 2);
+        psens(ifin,3) = -mean(em.diff(psens(ifin,:)));
+        
+        % Compute the difference in z for all points of the scan.
+        % Allow only positive values, as negative differences mean the scan
+        % hit an object and the corresponding point is correct.
+        dz = constrain(em.diff(psens), [0,+Inf]);
+        
+        % Compute the mean difference along z between the elevation map and 
+        % the scan.
+        if sum(isnan(dz)) < 0.7*numel(dz)
             d(ix,iy) = mean(dz, 'omitnan');
         end
         
         % Compute the fraction of unmatched points.
         nanfrac(ix,iy) = sum(isnan(dz)) / numel(dz);
     end
+    
+    % Advance the progress bar.
     progressbar
 end
 
-% Plot result.
-figure('Name', 'Sensor model in middle of field')
+%% Plot results.
+% Plot the mean z distance for all offsets.
+fig = figure('Name', 'Mean distance in z');
 surf(x, y, d', 'EdgeColor', 'none')
-daspect([1 1 0.01])
+%xlim([20,30])
+%ylim([5,15])
 labelaxes
-
-% Colorize field map according to visualize the sensor model output.
-% c = floor(normm(d) * 63.9999) + 1;
-% palette = uint8(round(colormap * 255));
-% roi = [lim(1,1), lim(1,2)-eps, lim(2,1), lim(2,2)-eps, -Inf, +Inf];
-% pcfieldsel = select(pcfield, findPointsInROI(pcfield, roi));
-% minxy = repmat(lim(1:2,1).', pcfieldsel.Count, 1);
-% i = floor((pcfieldsel.Location(:,1:2) - minxy) / res) + 1;
-% i = sub2ind(size(c), i(:,1), i(:,2));
-% pcfieldsel.Color = palette(c(i),:);
-% figure('Name', 'Field map showing robot location probability')
-% pcshow(pcfieldsel, 'MarkerSize', 40)
-
-% Show the fraction of missing correspondences.
-% figure('Name', 'NaN count')
-% surf(x, y, nanfrac', 'EdgeColor', 'none')
-% labelaxes
-
-% Plot point cloud of field and the scan point cloud at the most probable
-% location.
-[~,imin] = min(d(:));
-[xmin,ymin] = ind2sub(size(d), imin);
-offset = repmat([x(xmin),y(ymin),0], n, 1);
-offset(:,3) = -mean(em.diff(psens+offset), 'omitnan');
-figure('Name', 'Field')
-pcshowpair(pcfield, pointCloud(psens+offset), 'MarkerSize', 50)
-axis equal
-labelaxes
-
-% Vary yaw angle.
-yaw = -pi : 0.01 : pi;
-dyaw = NaN(numel(yaw), 1);
-for iyaw = 1 : numel(yaw)
-    pcsensrot = pctransform(pcsens, ...
-        ht2affine3d(eul2tform([yaw(iyaw),0,0])));
-
-    % Compute the disparity between elevation map and scan.
-    p = pcsensrot.Location + offset;
-    dz = constrain(em.diff(p), [0,+Inf]);
-    dyaw(iyaw) = mean(dz, 'omitnan');
-end
-figure('Name', 'Matching result of best position over yaw angle')
-plot(yaw, dyaw)
-
-% Vary pitch angle.
-pitch = -pi : 0.01 : pi;
-dpitch = NaN(numel(pitch), 1);
-for ipitch = 1 : numel(pitch)
-    pcsensrot = pctransform(pcsens, ...
-        ht2affine3d(eul2tform([0,pitch(ipitch),0])));
-    
-    % Compute the disparity between elevation map and scan.
-    p = pcsensrot.Location + offset;
-    dz = constrain(em.diff(p), [0;+Inf]);
-    dpitch(ipitch) = mean(dz, 'omitnan');
-end
-figure('Name', 'Matching result of best position over pitch angle')
-plot(pitch, dpitch)
-
-% Vary roll angle.
-roll = -pi : 0.01 : pi;
-droll = NaN(numel(roll), 1);
-for iroll = 1 : numel(roll)
-    pcsensrot = pctransform(pcsens, ...
-        ht2affine3d(eul2tform([0,0,roll(iroll)])));
-    
-    % Compute the disparity between elevation map and scan.
-    p = pcsensrot.Location + offset;
-    dz = constrain(em.diff(p), [0;+Inf]);
-    droll(iroll) = mean(dz, 'omitnan');
-end
-figure('Name', 'Matching result of best position over roll angle')
-plot(roll, droll)
