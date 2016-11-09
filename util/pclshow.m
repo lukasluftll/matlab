ceebd26ad9a41853cc4c870a30e20a8f43dddcb0
Alexander Schaefer <aschaef@cs.uni-freiburg.de>
2016-11-09 13:15:00 +0100

    Unset LD_LIBRARY_PATH in a more elegant way.

--- util/pclshow.m
+++ util/pclshow.m
@@ -7,7 +7,7 @@ cellfun(valfun, varargin)
 
 dirname = [tempdir, 'pclshow/'];
 if exist(dirname, 'dir') == 7
-    delete([dirname, '*.pcd']);
+    delete([dirname, '*']);
 else
     [success,msg,msgid] = mkdir(dirname);
     if ~success
@@ -18,7 +18,7 @@ end
 pcwritefun = @(x) pcwrite(x, [tempname(dirname), '.pcd']);
 cellfun(pcwritefun, varargin);
 
-status = system(['LD_LIBRARY_PATH=; pcl_viewer ', dirname, '*.pcd &']);
+status = system(['unset LD_LIBRARY_PATH; pcl_viewer ', dirname, '*.pcd &']);
 if status ~= 0
     error('Failed to launch pcl_viewer.')
 end
