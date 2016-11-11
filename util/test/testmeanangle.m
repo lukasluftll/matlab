0db424d3fa6c726d930435a1d5f86990a5d2c46f
Alexander Schaefer <aschaef@cs.uni-freiburg.de>
2016-11-11 23:54:34 +0100

    Add test cases for vectorsum method.

--- util/test/testmeanangle.m
+++ util/test/testmeanangle.m
@@ -8,17 +8,32 @@
 m = meanangle([]);
 assert(isempty(m));
 
-%% Equal angles.
+m = meanangle([], 'vectorsum');
+assert(isempty(m));
+
+%% Equal angles applied.
 m = meanangle(ones(2,30));
 assert(m == 1);
 
+m = meanangle(ones(2,30), 'vectorsum');
+assert(ismembertol(m, 1));
+
 %% Symmetrically distributed angles.
 m = meanangle(-1 : 0.01 : +1);
-assert(m == 0 || m == 2*pi);
+assert(m == 0);
 
 m = meanangle(-0.3 : 0.2 : +0.3);
-assert(m == 0 || m == 2*pi);
+assert(m == 0);
+
+m = meanangle(-1 : 0.01 : +1, 'vectorsum');
+assert(m < eps);
+
+m = meanangle(-0.3 : 0.2 : +0.3, 'vectorsum');
+assert(m < eps);
 
 %% Randomly distributed angles.
 m = meanangle(rand(1, 100));
+assert(0 < m && m < 1);
+
+m = meanangle(rand(1, 100), 'vectorsum');
 assert(0 < m && m < 1);
