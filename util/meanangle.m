bcc71cfb447c7d1018c5c201b6314739b6ac12c3
Alexander Schaefer <aschaef@cs.uni-freiburg.de>
2016-11-11 23:29:49 +0100

    Add vector sum computation method.

--- util/meanangle.m
+++ util/meanangle.m
@@ -1,11 +1,22 @@
-function m = meanangle(theta)
+function m = meanangle(theta, method)
 % MEANANGLE Mean of set of angles in radians.
-%   M = MEANANGLE(THETA) computes the mean of THETA. M minimizes the
-%   squared arc length error.
+%   M = MEANANGLE(THETA) computes the mean of THETA. 
 %
 %   THETA is a n-D matrix that contains a set of angles in radians.
 %
-%   M is the mean angle wrapped to [0; 2*pi].
+%   M is the mean angle wrapped to [-pi; +pi].
+%
+%   M = MEANANGLE(THETA, METHOD) specifies the algorithm used to compute
+%   the mean:
+%   'arcmin'    - Minimizes the squared arc lengths between the mean angle 
+%                 and the given angles when drawn as points on the unit
+%                 circle.
+%   'vectorsum' - Compute the unit vector corresponding to each angle, sum
+%                 up the vectors, and compute the arc tangent. This method
+%                 minimizes the squared chord lengths between the mean 
+%                 angle and the given angles when drawn as points on the 
+%                 unit circle.
+%   Default is 'arcmin'.
 %
 %   Example:
 %      meanangle([-0.5 : 0.1 : 0.2])
@@ -21,27 +32,46 @@ function m = meanangle(theta)
 % Shanghai, China.
 
 %% Validate input and output.
+% Check number of input and output arguments.
 nargoutchk(0, 1)
-narginchk(1, 1)
+narginchk(1, 2)
+
+% Check given angles.
 validateattributes(theta, {'numeric'}, {'real'}, '', 'THETA')
+theta = theta(:)';
+
+% Check given method.
+if nargin < 2
+    method = 'arcmin';
+end
+validateattributes(method, {'char'}, {'row', 'nonempty'}, '', 'METHOD')
 
 %% Compute mean.
-% Notation is inspired by Olson's paper.
+switch lower(method)
+    case 'arcmin'
+        % Use Olson's approach.
+        % Notation is inspired by Olson's paper.
 
-% Map all angles to [0; 2*pi] and sort them.
-theta = sort(wrapTo2Pi(theta(:)))';
+        % Map all angles to [0; 2*pi] and sort them.
+        theta = sort(wrapTo2Pi(theta(:)))';
 
-% Compute the moments for all angle arrangements.
-N = numel(theta);
-M1 = sum(theta) + (0:N-1) * 2*pi;
-M2 = sum(theta.^2) + [0, cumsum(4*pi*(theta(1:end-1) + pi))];
+        % Compute the moments for all angle arrangements.
+        N = numel(theta);
+        M1 = sum(theta) + (0:N-1) * 2*pi;
+        M2 = sum(theta.^2) + [0, cumsum(4*pi*(theta(1:end-1) + pi))];
 
-% Compute the mean and the variance for all arrangements.
-m = M1 / N;
-sigma = M2 - 2*M1.*m + N*m.^2;
+        % Compute the mean and the variance for all arrangements.
+        m = M1 / N;
+        sigma = M2 - 2*M1.*m + N*m.^2;
 
-% Return the mean that minimizes the squared error.
-[~,imin] = min(sigma);
-m = wrapTo2Pi(m(imin));
+        % Return the mean that minimizes the squared error.
+        [~,imin] = min(sigma);
+        m = wrapToPi(m(imin));
+    case 'vectorsum'
+        % Use the vector sum method.
+        m = atan2(sum(sin(theta)), sum(cos(theta)));
+    otherwise
+        error(['Unknown method ', upper(method), '.'])
+end
 
 end
